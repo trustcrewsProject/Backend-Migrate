b2d6ae2fb9c293fc3f117d093dc3fade2c7a3737
songhyeonpk <ehftozl1997@gmail.com>
2023-11-07 16:36:10 +0900

    [feature/first] 프로젝트 구성원 권한 엔티티 개발

---
+++ src/main/java/com/example/demo/model/ProjectMemberRole.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+// 프로젝트 구성원 역할 엔티티
+@Entity
+@Table(name = "project_member_role")
+public class ProjectMemberRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "project_member_role_id")
+    private Long id;
+
+    @Column(name = "project_member_role_name")
+    private String name;
+}

c154e9c7e7dbf6615808a123dafdc3b5b4f782ff
sunggun1 <readingdeveloper@naver.com>
2023-11-07 23:20:16 +0900

    [feature-first] TrustScore 롬복 추가

--- src/main/java/com/example/demo/model/TrustScore.java
+++ src/main/java/com/example/demo/model/TrustScore.java
@@ -1,8 +1,13 @@
 package com.example.demo.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "trust_score")
 public class TrustScore extends BaseTimeEntity{
     @Id

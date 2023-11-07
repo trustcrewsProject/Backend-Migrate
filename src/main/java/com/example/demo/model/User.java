1e56fa05e35f03089b68cf66bfede0ce9eb105e6
sunggun1 <readingdeveloper@naver.com>
2023-11-07 19:03:00 +0900

    [feature-first] user model getter 추가

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import com.example.demo.constant.Role;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
@@ -10,6 +11,7 @@ import javax.persistence.*;
 @Table(name = "user")
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class User extends BaseTimeEntity{
     @Id
     @Column(name = "user_id")

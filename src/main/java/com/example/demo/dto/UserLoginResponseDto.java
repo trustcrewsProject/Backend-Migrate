e865e4776a21c31305f9e3bce5e7d0f20b659af5
songhyeonpk <ehftozl1997@gmail.com>
2023-11-08 14:33:07 +0900

    [feature/first] 사용자 로그인 응답 DTO 생성

---
+++ src/main/java/com/example/demo/dto/UserLoginResponseDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.User;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class UserLoginResponseDto {
+
+    private Long userId;
+
+    private String email;
+
+    public static UserLoginResponseDto from (User user) {
+        return UserLoginResponseDto.builder()
+                .userId(user.getId())
+                .email(user.getEmail())
+                .build();
+    }
+}

8d9df4fa49bd4fc7889a5914cbb3acbd767e2281
songhyeonpk <ehftozl1997@gmail.com>
2023-11-14 16:55:38 +0900

    [feature/2-create-entitys] feat : 포지션 엔티티 구현

---
+++ src/main/java/com/example/demo/model/Position.java
@@ -0,0 +1,26 @@
+package com.example.demo.model;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Position {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "position_id")
+    private Long id;
+
+    private String name;
+
+    @Builder
+    private Position(String name) {
+        this.name = name;
+    }
+}

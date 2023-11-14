672aad73de7377c519ea0c4e66af792d2372d7e7
sunggun1 <readingdeveloper@naver.com>
2023-11-14 15:51:15 +0900

    [feature-2-create-entitys] AlertType, Alert  엔티티 , enum 추가

---
+++ src/main/java/com/example/demo/model/Alert.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import com.example.demo.constant.AlertType;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "alert")
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Alert extends BaseTimeEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "alert_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String content;
+
+    private AlertType type;
+
+    private boolean checked_YN;
+}

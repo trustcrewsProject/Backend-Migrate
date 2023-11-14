a3fffc0c2e59435e0f90bfc18935b1edbca9977b
sunggun1 <readingdeveloper@naver.com>
2023-11-14 15:27:24 +0900

    [fetaure/2-create-entitys] DTO 만든거 모두 삭제, 서비스, 리포지토리, 컨트롤러 모두 삭제

--- src/main/java/com/example/demo/repository/BoardPositionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.BoardPosition;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardPositionRepository extends JpaRepository<BoardPosition, Long> {
-}
--- src/main/java/com/example/demo/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-}

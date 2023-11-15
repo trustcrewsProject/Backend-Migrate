b8a95981263a423a11ea493c5f0b60601b3127f0
sunggun1 <readingdeveloper@naver.com>
2023-11-15 19:21:31 +0900

    [develop] @controller, @service 추가

--- src/main/java/com/example/demo/controller/TrustGradeController.java
+++ src/main/java/com/example/demo/controller/TrustGradeController.java
@@ -1,4 +1,8 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TrustGradeController {
 }

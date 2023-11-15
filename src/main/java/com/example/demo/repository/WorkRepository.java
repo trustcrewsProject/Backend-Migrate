0a3da55c0452f6fe190f202cc1d0d191fc7e1ce1
sunggun1 <readingdeveloper@naver.com>
2023-11-15 14:45:25 +0900

    [feature-3-create-body-entity] 리포지토리 작성

---
+++ src/main/java/com/example/demo/repository/WorkRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Work;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkRepository extends JpaRepository<Work, Long> {
+}

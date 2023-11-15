1db4fc00d46574de1f87553a5a253546c82d035b
songhyeonpk <ehftozl1997@gmail.com>
2023-11-15 17:14:37 +0900

    [feature/6-layer-design] feat : 직무 서비스 생성

---
+++ src/main/java/com/example/demo/service/PositionServiceImpl.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.PositionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PositionServiceImpl implements PositionService{
+
+    private final PositionRepository positionRepository;
+}

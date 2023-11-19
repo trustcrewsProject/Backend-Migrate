5e8efd11d177721994ddbd59acffa641705b7e33
joo <rkdepik@gmail.com>
2023-11-19 16:24:33 +0900

    changed dto name from 'change' to 'update'

--- src/main/java/com/example/demo/dto/trustscore/request/TrustScoreUpdateRequestDto.java
+++ src/main/java/com/example/demo/dto/trustscore/request/TrustScoreUpdateRequestDto.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.NotNull;
 @Getter
 @Setter
 @NoArgsConstructor
-public class TrustScoreChangeRequestDto {
+public class TrustScoreUpdateRequestDto {
     @NotNull
     Long userId;
     Long projectId;

43d67e5ea698bc4219a7813ec824bbc5fc22478b
songhyeonpk <ehftozl1997@gmail.com>
2023-11-19 23:09:40 +0900

    [feature/6-security-config] feat : 시큐리티 컨텍스트 접근을 위한 SecurityUtil 클래스 구현

---
+++ src/main/java/com/example/demo/util/SecurityUtil.java
@@ -0,0 +1,21 @@
+package com.example.demo.util;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SecurityUtil {
+
+    // 시큐리티 컨텍스트에 저장된 회원조회
+    public String getCurrentUserEmail() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication != null || authentication.getPrincipal() instanceof UserDetails) {
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            return userDetails.getUsername();
+        }
+
+        return null;
+    }
+}

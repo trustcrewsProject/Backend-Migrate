fb0dd9f7aeab38685a147f4290928589c7d26aed
songhyeonpk <ehftozl1997@gmail.com>
2023-11-20 17:56:26 +0900

    move : QueryDslConfig 파일과 util 패키지 global 패키지로 이동

--- src/main/java/com/example/demo/global/util/SecurityUtil.java
+++ src/main/java/com/example/demo/global/util/SecurityUtil.java
@@ -1,4 +1,4 @@
-package com.example.demo.util;
+package com.example.demo.global.util;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;

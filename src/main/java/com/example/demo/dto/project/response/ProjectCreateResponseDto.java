1a563dcc82d9c43054832e1a4e0ce9f7b5f48b66
sunggun1 <readingdeveloper@naver.com>
2023-11-22 12:51:41 +0900

    [feature/3-create-board-project-resposne-dto] ProjectCreateResponseDto 추가

---
+++ src/main/java/com/example/demo/dto/project/response/ProjectCreateResponseDto.java
@@ -0,0 +1,37 @@
+package com.example.demo.dto.project.response;
+
+import com.example.demo.constant.ProjectStatus;
+import com.example.demo.model.Project;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProjectCreateResponseDto {
+    private String name;
+    private String subject;
+    private long trustGradeId;
+    private long userId;
+    private ProjectStatus status;
+    private int crewNumber;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private LocalDateTime createDate;
+    private LocalDateTime updateDate;
+
+    public static ProjectCreateResponseDto of(Project project) {
+        return ProjectCreateResponseDto.builder()
+                .name(project.getName())
+                .subject(project.getSubject())
+                .trustGradeId(project.getTrustGrade().getId())
+                .userId(project.getUser().getId())
+                .status(project.getStatus())
+                .crewNumber(project.getCrewNumber())
+                .startDate(project.getStartDate())
+                .endDate(project.getEndDate())
+                .createDate(project.getCreateDate())
+                .updateDate(project.getUpdateDate())
+                .build();
+    }
+}

85d8cb07027718a78c2f2719bbb006b64d49504e
songhyeonpk <ehftozl1997@gmail.com>
2023-11-23 22:13:47 +0900

    fix : 전역 예외 핸들링 코드 수정

--- src/main/java/com/example/demo/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/global/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.demo.global.exception;
 
+import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.global.exception.customexception.CustomException;
 import com.example.demo.global.exception.errorcode.CommonErrorCode;
 import org.springframework.http.HttpStatus;
@@ -8,21 +9,28 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     // @Valid 어노테이션 유효성 검사 실패 시 발생 Exception
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> processValidationError(MethodArgumentNotValidException e) {
-        final ErrorResponse response =
-                ErrorResponse.of(CommonErrorCode.INVALID_VALUE, e.getBindingResult());
+    public ResponseEntity<ResponseDto<?>> processValidationError(MethodArgumentNotValidException e) {
+        List<ResponseDto.CustomFieldError> errors = e.getFieldErrors().stream()
+                .filter(fieldError -> fieldError != null)
+                .map(ResponseDto.CustomFieldError::new).collect(Collectors.toList());
+
+        final ResponseDto<List<ResponseDto.CustomFieldError>> response =
+                ResponseDto.fail("", errors);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 
     // CustomException
     @ExceptionHandler(CustomException.class)
-    public ResponseEntity<ErrorResponse> customError(CustomException e) {
-        final ErrorResponse response = ErrorResponse.from(e.getErrorCode());
+    public ResponseEntity<ResponseDto<?>> customExceptionHandle(CustomException e) {
+        final ResponseDto response = ResponseDto.fail(e.getErrorCode().getMessage());
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
     }
 }

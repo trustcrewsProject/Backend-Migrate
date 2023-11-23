1bf40ef2f64fe0f3d44768c63c8ac10e0d055e23
songhyeonpk <ehftozl1997@gmail.com>
2023-11-23 22:13:22 +0900

    fix : 공통 응답 DTO ResponseDto 수정

--- src/main/java/com/example/demo/dto/common/ResponseDto.java
+++ src/main/java/com/example/demo/dto/common/ResponseDto.java
@@ -1,10 +1,68 @@
 package com.example.demo.dto.common;
 
 import lombok.*;
+import org.springframework.validation.FieldError;
 
 @Getter
-@AllArgsConstructor
-public class ResponseDto<D> {
+public class ResponseDto<T> {
+
+    private String result;
     private String message;
-    private D data;
+    private T data;
+
+    @Builder
+    public ResponseDto(String result, String message, T data) {
+        this.result = result;
+        this.message = message;
+        this.data = data;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CustomFieldError {
+
+        private String field;
+        private String invalidValue;
+        private String reason;
+
+        public CustomFieldError(FieldError fieldError) {
+            this.field = fieldError.getField();
+            this.invalidValue = fieldError.getRejectedValue().toString();
+            this.reason = fieldError.getDefaultMessage();
+        }
+    }
+
+    // 요청 성공 시, 응답 데이터가 없는 경우
+    public static ResponseDto<Void> success(String message) {
+        return ResponseDto.<Void>builder()
+                .result("success")
+                .message(message)
+                .build();
+    }
+
+    // 요청 성공 시, 응답 데이터가 있는 경우
+    public static <T> ResponseDto<T> success(String message, T data) {
+        return ResponseDto.<T>builder()
+                .result("success")
+                .message(message)
+                .data(data)
+                .build();
+    }
+
+    // 요청 실패 시, 응답 데이터가 없는 경우
+    public static ResponseDto<Void> fail(String message) {
+        return ResponseDto.<Void>builder()
+                .result("fail")
+                .message(message)
+                .build();
+    }
+
+    // 요청 실패 시, 응답 데이터가 있는 경우 (데이터 유효성 검사)
+    public static <T> ResponseDto<T> fail(String message, T errors) {
+        return ResponseDto.<T>builder()
+                .result("fail")
+                .message(message)
+                .data(errors)
+                .build();
+    }
 }

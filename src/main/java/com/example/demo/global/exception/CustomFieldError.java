28f5f3ae4e5349cafe82a26b696ef09f44b9205b
songhyeonpk <ehftozl1997@gmail.com>
2023-11-23 23:07:26 +0900

    feat : CustomFieldError 클래스 생성

---
+++ src/main/java/com/example/demo/global/exception/CustomFieldError.java
@@ -0,0 +1,20 @@
+package com.example.demo.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.validation.FieldError;
+
+@Getter
+@AllArgsConstructor
+public class CustomFieldError {
+
+    private String field;
+    private String invalidValue;
+    private String reason;
+
+    public CustomFieldError(FieldError fieldError) {
+        this.field = fieldError.getField();
+        this.invalidValue = fieldError.getRejectedValue().toString();
+        this.reason = fieldError.getDefaultMessage();
+    }
+}

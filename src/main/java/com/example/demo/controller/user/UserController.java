9a00a31a4e3fde2039cfbc8f86054de67b38d987
songhyeonpk <ehftozl1997@gmail.com>
2023-11-24 14:02:52 +0900

    [feature/25-check-email] feat : UserController에 이메일 중복확인 요청 API 추가

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller.user;
 
+import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    // 이메일 중복확인 요청
+    @GetMapping("/api/user/check-email/{email}")
+    public ResponseEntity<ResponseDto<?>> checkEmail(@PathVariable String email) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(userService.checkEmail(email));
+    }
 }

9d9c94b869876b3d4875bfd619b36234c89a5a76
songhyeonpk <ehftozl1997@gmail.com>
2023-11-24 13:56:04 +0900

    [feature/25-check-email] feat : 이메일 중복 에러코드 추가

--- src/main/java/com/example/demo/global/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/example/demo/global/exception/errorcode/UserErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum UserErrorCode implements ErrorCode {
-    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다.");
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다."),
+    ALREADY_EMAIL(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다.");
 
     private HttpStatus status;
     private String message;

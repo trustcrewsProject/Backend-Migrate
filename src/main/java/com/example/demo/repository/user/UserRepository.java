7c6b36171664534c6fc159fa345e6bd546d0552f
songhyeonpk <ehftozl1997@gmail.com>
2023-11-24 13:59:20 +0900

    [feature/25-check-email] feat : UserRepository에 existsByEmail() 추가

--- src/main/java/com/example/demo/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -3,4 +3,8 @@ package com.example.demo.repository.user;
 import com.example.demo.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    // 이메일 확인
+    boolean existsByEmail(String email);
+}

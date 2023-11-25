3ab26a58a7cfb89c9edd89ce5b2f77c338d652ac
sunggun1 <readingdeveloper@naver.com>
2023-11-25 15:00:40 +0900

    [feature/3-create-board-controller] 검색 부분 컨트롤러, DTO 개발  BoardSearchResponseDto, ProjectSearchResponseDto, TrustGradeResponseDto, UserSearchResponseDto 개발

--- src/main/java/com/example/demo/controller/board/BoardController.java
+++ src/main/java/com/example/demo/controller/board/BoardController.java
@@ -1,6 +1,29 @@
 package com.example.demo.controller.board;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.board.request.BoardSearchRequestDto;
+import com.example.demo.dto.board.response.BoardSearchResponseDto;
+import com.example.demo.dto.board_project.request.BoardProjectCreateRequestDto;
+import com.example.demo.dto.board_project.response.BoardProjectCreateResponseDto;
+import com.example.demo.dto.common.ResponseDto;
+
+import com.example.demo.service.board.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-public class BoardController {}
+@RequiredArgsConstructor
+@RequestMapping("/api/board")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @PostMapping("/search")
+    public ResponseEntity<ResponseDto<?>> get(@RequestBody BoardSearchRequestDto dto) {
+        List<BoardSearchResponseDto> result = boardService.search(dto);
+        return new ResponseEntity<>(new ResponseDto<>("success", result), HttpStatus.OK);
+    }
+}

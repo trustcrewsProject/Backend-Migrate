f87965df560e625e2abce693dcbb024aee139ecd
sunggun1 <readingdeveloper@naver.com>
2023-11-25 16:55:11 +0900

    [feature/3-create-board-controller] 게시글 생성 게시글 생성 컨트롤러 유저 익셉션 수정 게시글 생성 서비스 수정 신용등급 익셉션 생성

--- src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
@@ -7,6 +7,9 @@ public class UserCustomException extends CustomException {
     public static final UserCustomException ALREADY_EMAIL =
             new UserCustomException(UserErrorCode.ALREADY_EMAIL);
 
+    public static final UserCustomException NOT_FOUND_USER =
+            new UserCustomException(UserErrorCode.NOT_FOUND_USER);
+
     public UserCustomException(UserErrorCode userErrorCode) {
         super(userErrorCode);
     }

04ebd10dcc2a8548b8b8ff9018edac402eaa3a73
jkang1201 <148705540+jkang1201@users.noreply.github.com>
2023-11-26 21:34:44 +0900

    [feature/19-trust-score-api-conflict] 신뢰점수 관련 Controller 개발
    신뢰점수 관련 Controller 개발

--- src/main/java/com/example/demo/controller/trust_score/TrustScoreController.java
+++ src/main/java/com/example/demo/controller/trust_score/TrustScoreController.java
@@ -1,6 +1,26 @@
 package com.example.demo.controller.trust_score;
 
+import com.example.demo.dto.common.ResponseDto;
+import com.example.demo.dto.trust_score.request.TrustScoreUpdateRequestDto;
+import com.example.demo.dto.trust_score.response.TrustScoreUpdateResponseDto;
+import com.example.demo.service.trust_score.TrustScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RestController
-public class TrustScoreController {}
+@RequiredArgsConstructor
+public class TrustScoreController {
+    private final TrustScoreService trustScoreService;
+    @PostMapping("/api/trust-score")
+    public ResponseEntity<ResponseDto<?>> updateScoreByManager(
+            @RequestBody @Valid TrustScoreUpdateRequestDto requestDto) {
+        TrustScoreUpdateResponseDto responseDto = trustScoreService.updateTrustScore(requestDto);
+        return new ResponseEntity<>(new ResponseDto<>("HttpStatusOK", "success", responseDto), HttpStatus.OK);
+    }
+}

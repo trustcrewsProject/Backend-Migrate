17e8a943dc3b8bb6564361b6c63a4074a25aa3d8
songhyeonpk <ehftozl1997@gmail.com>
2023-11-26 18:29:49 +0900

    [feature/25-get-technology-stack-list] feat : TechnologyStackController에 기술스택 목록 조회 요청 api 추가

--- src/main/java/com/example/demo/controller/technology_stack/TechnologyStackController.java
+++ src/main/java/com/example/demo/controller/technology_stack/TechnologyStackController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller.technology_stack;
 
+import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.service.technology_stack.TechnologyStackService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class TechnologyStackController {
 
     private final TechnologyStackService technologyStackService;
+
+    // 기술스택 목록 조회 요청
+    @GetMapping("/api/technology-stack-list")
+    public ResponseEntity<ResponseDto<?>> getTechnologyStackList() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(technologyStackService.getTechnologyStackList());
+    }
 }

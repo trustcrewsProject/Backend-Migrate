e12ea687f292d79dbced9d333c2e52c3b790cc95
songhyeonpk <ehftozl1997@gmail.com>
2023-11-26 18:18:34 +0900

    [feature/25-get-position-list] feat : PositionController에 포지션 목록 조회 요청 api 추가

--- src/main/java/com/example/demo/controller/position/PositionController.java
+++ src/main/java/com/example/demo/controller/position/PositionController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller.position;
 
+import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.service.position.PositionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PositionController {
 
     private final PositionService positionService;
+
+    // 포지션 목록 조회 요청
+    @GetMapping("/api/position-list")
+    public ResponseEntity<ResponseDto<?>> getPositionList() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(positionService.getPositionList());
+    }
 }

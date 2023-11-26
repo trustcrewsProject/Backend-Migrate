5785639257c687565520b1652e4eaff38dcdf10b
sunggun1 <readingdeveloper@naver.com>
2023-11-26 17:05:22 +0900

    [feature/6-create-project-member] 특정 프로젝트 멤버 탈퇴 기능 (탈퇴 알림 발송)

--- src/main/java/com/example/demo/controller/project/ProjectMemberController.java
+++ src/main/java/com/example/demo/controller/project/ProjectMemberController.java
@@ -1,6 +1,25 @@
 package com.example.demo.controller.project;
 
+import com.example.demo.dto.common.ResponseDto;
+import com.example.demo.service.project.ProjectMemberService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProjectMemberController {}
+@RequestMapping("/api/projectmember")
+@AllArgsConstructor
+public class ProjectMemberController {
+
+    private final ProjectMemberService projectMemberService;
+
+    @PostMapping("/{projectMemberId}/withdrawl")
+    public ResponseEntity<ResponseDto<?>> withdrawl(@PathVariable("projectMemberId") Long projectMemberId) {
+        projectMemberService.sendWithdrawlAlert(projectMemberId);
+        return new ResponseEntity<>(ResponseDto.success("success"), HttpStatus.OK);
+    }
+}

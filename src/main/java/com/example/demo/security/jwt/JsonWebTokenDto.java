7dd2c307895c0b70b1457e972c3581579fbde58a
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 14:44:24 +0900

    [feature/74-jwt] feat : JsonWebTokenDto 생성

---
+++ src/main/java/com/example/demo/security/jwt/JsonWebTokenDto.java
@@ -0,0 +1,19 @@
+package com.example.demo.security.jwt;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class JsonWebTokenDto {
+
+    private String accessToken;
+    private String refreshToken;
+
+    public static JsonWebTokenDto of (String accessToken, String refreshToken) {
+        return JsonWebTokenDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}

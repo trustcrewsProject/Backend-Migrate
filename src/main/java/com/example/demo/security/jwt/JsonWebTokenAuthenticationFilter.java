c42bbda4ec765802af2f81b19fbfcfc9ae838544
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 17:53:52 +0900

    [feature/74-security-authentication] feat : 로그인 요청 시 수행되는 JsonWebTokenAuthenticationFilter의  attaempAuthentication() 메소드 구현

--- src/main/java/com/example/demo/security/jwt/JsonWebTokenAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/jwt/JsonWebTokenAuthenticationFilter.java
@@ -1,6 +1,12 @@
 package com.example.demo.security.jwt;
 
 
+import com.example.demo.dto.user.request.UserLoginRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -13,13 +19,32 @@ import java.io.IOException;
 
 // 인증 필터
 // 인증에 성공하면 JWT 발급
+@RequiredArgsConstructor
+@Slf4j
 public class JsonWebTokenAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private final AuthenticationManager authenticationManager;
+
+    // 로그인을 요청했을 때 실행되는 로직
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        ObjectMapper objectMapper = new ObjectMapper();
+        UserLoginRequestDto loginRequest = null;
+
+        try {
+            loginRequest = objectMapper.readValue(request.getInputStream(), UserLoginRequestDto.class);
+        } catch (Exception e) {
+            log.debug(e.getMessage());
+        }
+
+        // 토큰생성
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+        Authentication authentication = authenticationManager.authenticate(token);
+
+        return authentication;
     }
 
+    // 인증에 성공했을 경우 실행되는 로직
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Authentication authResult) throws IOException, ServletException {
         super.successfulAuthentication(request, response, filterChain, authResult);

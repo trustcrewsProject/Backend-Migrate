2f9f2bdc61cb9681c75213077d2588bed9d35883
sunggun1 <readingdeveloper@naver.com>
2023-11-28 18:01:30 +0900

    [feature/6-refactoring-board] 게시글 상세

--- src/main/java/com/example/demo/service/board/BoardServiceImpl.java
+++ src/main/java/com/example/demo/service/board/BoardServiceImpl.java
@@ -2,14 +2,27 @@ package com.example.demo.service.board;
 
 import com.example.demo.dto.board.request.BoardSearchRequestDto;
 import com.example.demo.dto.board.response.*;
+import com.example.demo.dto.boardposition.BoardPositionDetailResponseDto;
+import com.example.demo.dto.position.response.PositionResponseDto;
+import com.example.demo.dto.project.response.ProjectDetailResponseDto;
+import com.example.demo.dto.technology_stack.response.TechnologyStackInfoResponseDto;
+import com.example.demo.dto.trust_grade.response.TrustGradeResponseDto;
+import com.example.demo.dto.user.response.UserBoardDetailResponseDto;
+import com.example.demo.dto.user.response.UserProjectResponseDto;
 import com.example.demo.global.exception.customexception.*;
 import com.example.demo.model.board.Board;
+import com.example.demo.model.board.BoardPosition;
+import com.example.demo.model.project.ProjectTechnology;
+import com.example.demo.model.technology_stack.TechnologyStack;
 import com.example.demo.repository.board.BoardRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Transactional
 @Service
@@ -41,37 +54,37 @@ public class BoardServiceImpl implements BoardService {
      * @param boardId
      * @return
      */
-//    public BoardTotalDetailResponseDto getDetail(Long boardId) {
-//        Board board = findBoardById(boardId);
-//
-//        //boardDetailResponseDto 생성
-//        UserBoardDetailResponseDto userBoardDetailResponseDto = UserBoardDetailResponseDto.of(board.getUser());
-//
-//        List<BoardPositionDetailResponseDto> boardPositionDetailResponseDtos = new ArrayList<>();
-//        for (BoardPosition boardPosition : board.getPositions()) {
-//            PositionResponseDto positionResponseDto = PositionResponseDto.of(boardPosition.getPosition());
-//            BoardPositionDetailResponseDto boardPositionDetailResponseDto = BoardPositionDetailResponseDto.of(boardPosition, positionResponseDto);
-//            boardPositionDetailResponseDtos.add(boardPositionDetailResponseDto);
-//        }
-//        BoardDetailResponseDto boardDetailResponseDto = BoardDetailResponseDto.of(board, userBoardDetailResponseDto, boardPositionDetailResponseDtos);
-//
-//        // ProjectDetailResponseDto 부분
-//        TrustGradeResponseDto trustGradeDto = TrustGradeResponseDto.of(board.getProject().getTrustGrade());
-//        UserProjectResponseDto userProjectResponseDto = UserProjectResponseDto.of(board.getProject());
-//
-//        //기술 스택 부분
-//        List<TechnologyStackInfoResponseDto> technologyStackInfoResponseDtos = new ArrayList<>();
-//        for (ProjectTechnology projectTechnology : board.getProject().getProjectTechnologies()) {
-//            TechnologyStack technologyStack = projectTechnology.getTechnologyStack();
-//
-//            TechnologyStackInfoResponseDto technologyStackInfoResponseDto = TechnologyStackInfoResponseDto.of(technologyStack.getId(), technologyStack.getName());
-//            technologyStackInfoResponseDtos.add(technologyStackInfoResponseDto);
-//        }
-//        ProjectDetailResponseDto projectDetailResponseDto = ProjectDetailResponseDto.of(board.getProject(), trustGradeDto, userProjectResponseDto, technologyStackInfoResponseDtos);
-//
-//
-//        return BoardTotalDetailResponseDto.of(boardDetailResponseDto, projectDetailResponseDto);
-//    }
+    public BoardTotalDetailResponseDto getDetail(Long boardId) {
+        Board board = findBoardById(boardId);
+
+        //boardDetailResponseDto 생성
+        UserBoardDetailResponseDto userBoardDetailResponseDto = UserBoardDetailResponseDto.of(board.getUser());
+
+        List<BoardPositionDetailResponseDto> boardPositionDetailResponseDtos = new ArrayList<>();
+        for (BoardPosition boardPosition : board.getPositions()) {
+            PositionResponseDto positionResponseDto = PositionResponseDto.of(boardPosition.getPosition());
+            BoardPositionDetailResponseDto boardPositionDetailResponseDto = BoardPositionDetailResponseDto.of(boardPosition, positionResponseDto);
+            boardPositionDetailResponseDtos.add(boardPositionDetailResponseDto);
+        }
+        BoardDetailResponseDto boardDetailResponseDto = BoardDetailResponseDto.of(board, userBoardDetailResponseDto, boardPositionDetailResponseDtos);
+
+        // ProjectDetailResponseDto 부분
+        TrustGradeResponseDto trustGradeDto = TrustGradeResponseDto.of(board.getProject().getTrustGrade());
+        UserProjectResponseDto userProjectResponseDto = UserProjectResponseDto.of(board.getProject());
+
+        //기술 스택 부분
+        List<TechnologyStackInfoResponseDto> technologyStackInfoResponseDtos = new ArrayList<>();
+        for (ProjectTechnology projectTechnology : board.getProject().getProjectTechnologies()) {
+            TechnologyStack technologyStack = projectTechnology.getTechnologyStack();
+
+            TechnologyStackInfoResponseDto technologyStackInfoResponseDto = TechnologyStackInfoResponseDto.of(technologyStack.getId(), technologyStack.getName());
+            technologyStackInfoResponseDtos.add(technologyStackInfoResponseDto);
+        }
+        ProjectDetailResponseDto projectDetailResponseDto = ProjectDetailResponseDto.of(board.getProject(), trustGradeDto, userProjectResponseDto, technologyStackInfoResponseDtos);
+
+
+        return BoardTotalDetailResponseDto.of(boardDetailResponseDto, projectDetailResponseDto);
+    }
 
     /**
      * 게시글, 프로젝트 생성, 프로젝트 기술 생성, 프로젝트 멤버 생성, 사용자 이력 생성, 게시글-포지션 생성

1d5188b0b988456af716570ca968b6b519b5c2da
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 16:51:28 +0900

    [feature/74-security-authentication] add : 사용자 인증 에러코드 추가

--- src/main/java/com/example/demo/global/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/example/demo/global/exception/errorcode/UserErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorCode implements ErrorCode {
     NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저가 존재하지 않습니다."),
     ALREADY_EMAIL(HttpStatus.CONFLICT, "이미 사용중인 이메일입니다."),
-    ALREADY_NICKNAME(HttpStatus.CONFLICT, "이미 사용중인 닉네임입니다.");
+    ALREADY_NICKNAME(HttpStatus.CONFLICT, "이미 사용중인 닉네임입니다."),
+    INVALID_AUTHENTICATION(HttpStatus.BAD_REQUEST, "사용자 인증에 실패하였습니다. 이메일 혹은 비밀번호를 확인해주세요.");
 
     private HttpStatus status;
     private String message;

3c10658d95e24f8cac74cc63bb07eaee070d6879
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 16:52:04 +0900

    [feature/74-security-authentication] add : 사용자 인증 커스텀 예외 추가

--- src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
@@ -12,6 +12,9 @@ public class UserCustomException extends CustomException {
   
     public static final UserCustomException ALREADY_NICKNAME =
             new UserCustomException(UserErrorCode.ALREADY_NICKNAME);
+
+    public static final UserCustomException INVALID_AUTHENTICATION =
+            new UserCustomException(UserErrorCode.INVALID_AUTHENTICATION);
   
     public UserCustomException(UserErrorCode userErrorCode) {
         super(userErrorCode);

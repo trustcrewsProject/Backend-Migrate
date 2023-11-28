8f2f513d1788be29cfdffd09d50888e589ece453
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 15:14:38 +0900

    [feature/74-jwt] feat : JWT 관련 커스텀 예외 추가

---
+++ src/main/java/com/example/demo/global/exception/customexception/TokenCustomException.java
@@ -0,0 +1,25 @@
+package com.example.demo.global.exception.customexception;
+
+import com.example.demo.global.exception.errorcode.TokenErrorCode;
+
+public class TokenCustomException extends CustomException{
+
+    public static final TokenCustomException NON_ACCESS_TOKEN =
+            new TokenCustomException(TokenErrorCode.NON_ACCESS_TOKEN);
+
+    public static final TokenCustomException WRONG_TYPE_SIGNATURE =
+            new TokenCustomException(TokenErrorCode.WRONG_TYPE_SIGNATURE);
+
+    public static final TokenCustomException WRONG_TYPE_TOKEN =
+            new TokenCustomException(TokenErrorCode.WRONG_TYPE_TOKEN);
+
+    public static final TokenCustomException EXPIRED_TOKEN =
+            new TokenCustomException(TokenErrorCode.EXPIRED_TOKEN);
+
+    public static final TokenCustomException MALFORMED_TOKEN =
+            new TokenCustomException(TokenErrorCode.MALFORMED_TOKEN);
+
+    public TokenCustomException(TokenErrorCode tokenErrorCode) {
+        super(tokenErrorCode);
+    }
+}

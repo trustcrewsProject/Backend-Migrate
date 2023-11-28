78f015251ee91142491502d8ea4e547abd38f4b1
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 14:18:26 +0900

    [feature/74-jwt] feat : JWT 관련 로직을 담당하는 JsonWebTokenProvider 생성 및 초기셋팅

---
+++ src/main/java/com/example/demo/security/jwt/JsonWebTokenProvider.java
@@ -0,0 +1,50 @@
+package com.example.demo.security.jwt;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.io.Encoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+@Slf4j
+@Component
+public class JsonWebTokenProvider {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String REFRESH_HEADER = "Refresh";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    // public static final long ACCESS_TOKEN_EXPIRE_MILLIS_TEST = 1000 * 60; // 1분
+    public static final long ACCESS_TOKEN_EXPIRE_MILLIS = 1000 * 60 * 30; // 30분
+
+    // public static final long REFRESH_TOKEN_EXPIRE_MILLIS_TEST = 1000 * 60 * 2; // 2분
+    public static final long REFRESH_TOKEN_EXPIRE_MILLIS = 1000 * 60 * 60 * 24 * 7; // 7일
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private Key key;
+
+    // key 초기화
+    @PostConstruct
+    public void init() {
+        String base64EncodedSecretKey = encodedBase64SecretKey(this.secretKey);
+        this.key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+    }
+
+    // JWT Secret Key Base64 Encoded
+    private String encodedBase64SecretKey(String secretKey) {
+        return Encoders.BASE64.encode(secretKey.getBytes(StandardCharsets.UTF_8));
+    }
+
+    // Base64로 인코드된 Secret Key Decoded -> key 셋팅
+    private Key getKeyFromBase64EncodedKey(String base64EncodedSecretKey) {
+        byte[] keyBytes = Decoders.BASE64URL.decode(base64EncodedSecretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}

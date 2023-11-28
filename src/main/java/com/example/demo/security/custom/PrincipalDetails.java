2dcf99fc28249ae1b13fcd1d05b12f3ad25b722b
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 16:53:05 +0900

    [feature/74-security-authentication] feat : PrincipalDetails 구현

--- src/main/java/com/example/demo/security/custom/PrincipalDetails.java
+++ src/main/java/com/example/demo/security/custom/PrincipalDetails.java
@@ -1,43 +1,77 @@
 package com.example.demo.security.custom;
 
+import com.example.demo.constant.Role;
+import com.example.demo.model.user.User;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
+@Getter
 public class PrincipalDetails implements UserDetails {
+
+    private Long id;
+    private String email;
+    private String password;
+    private String nickname;
+    private String role;
+
+    public PrincipalDetails(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.role = user.getRole().name();
+    }
+
+    public PrincipalDetails(String id, String email, String nickname, String role) {
+        this.id = Long.valueOf(id);
+        this.email = email;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public static PrincipalDetails of (String id, String email, String nickname, String role) {
+        return new PrincipalDetails(id, email, nickname, role);
+    }
+
+    // 회원 권한 정보 반환
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return Collections.singleton(new SimpleGrantedAuthority("ROLE_" + this.role));
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 
+    // Username 으로 회원의 PK 반환
     @Override
     public String getUsername() {
-        return null;
+        return String.valueOf(this.id);
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }

4d931096f4545effc9b18b347d9a7176f4ba91b1
songhyeonpk <ehftozl1997@gmail.com>
2023-11-28 15:14:15 +0900

    [feature/74-jwt] feat : JWT 관련 error code 추가

---
+++ src/main/java/com/example/demo/global/exception/errorcode/TokenErrorCode.java
@@ -0,0 +1,29 @@
+package com.example.demo.global.exception.errorcode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum TokenErrorCode implements ErrorCode{
+
+    NON_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "엑세스 토큰 정보가 존재하지 않습니다."),
+    WRONG_TYPE_SIGNATURE(HttpStatus.UNAUTHORIZED, "잘못된 JWT 시그니처입니다."),
+    WRONG_TYPE_TOKEN(HttpStatus.UNAUTHORIZED, "지원되지 않는 형식이나 구성의 토큰입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰 정보입니다."),
+    MALFORMED_TOKEN(HttpStatus.UNAUTHORIZED, "손상된 토큰 정보입니다.");
+
+    private HttpStatus status;
+    private String message;
+
+    @Override
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}

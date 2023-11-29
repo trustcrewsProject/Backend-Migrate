27bc2706f23ac11c4edb2aa440504e4c462c8539
songhyeonpk <ehftozl1997@gmail.com>
2023-11-29 23:24:47 +0900

    [feature/74-fix-user-login-dto] fix : UserLoginRequestDto @NoArgsConstructor 추가

--- src/main/java/com/example/demo/dto/user/request/UserLoginRequestDto.java
+++ src/main/java/com/example/demo/dto/user/request/UserLoginRequestDto.java
@@ -2,8 +2,10 @@ package com.example.demo.dto.user.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserLoginRequestDto {
 

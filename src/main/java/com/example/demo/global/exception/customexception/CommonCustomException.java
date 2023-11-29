d6411bbfd1fde06d507e1f3fc01bac834c6b5294
songhyeonpk <ehftozl1997@gmail.com>
2023-11-29 16:45:50 +0900

    add : 공통 500 에러 코드 및 예외 추가

--- src/main/java/com/example/demo/global/exception/customexception/CommonCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/CommonCustomException.java
@@ -7,6 +7,9 @@ public class CommonCustomException extends CustomException {
     public static final CommonCustomException DOES_NOT_EXIST_PARAMETER_IN_METHOD =
             new CommonCustomException(CommonErrorCode.DOES_NOT_EXIST_PARAMETER_IN_METHOD);
 
+    public static final CommonCustomException INTERNAL_SERVER_ERROR =
+            new CommonCustomException(CommonErrorCode.INTERNAL_SERVER_ERROR);
+
     public CommonCustomException(CommonErrorCode commonErrorCode) {
         super(commonErrorCode);
     }
--- src/main/java/com/example/demo/global/exception/errorcode/CommonErrorCode.java
+++ src/main/java/com/example/demo/global/exception/errorcode/CommonErrorCode.java
@@ -7,7 +7,8 @@ import org.springframework.http.HttpStatus;
 public enum CommonErrorCode implements ErrorCode {
     INVALID_VALUE(HttpStatus.BAD_REQUEST, "잘못된 형식의 데이터입니다."),
     DOES_NOT_EXIST_PARAMETER_IN_METHOD(
-            HttpStatus.BAD_REQUEST, "바인딩될 파라미터가 해당 메소드에 존재하지 않습니다. 메소드의 파라미터를 확인해주세요,");
+            HttpStatus.BAD_REQUEST, "바인딩될 파라미터가 해당 메소드에 존재하지 않습니다. 메소드의 파라미터를 확인해주세요,"),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal Server Error.");
 
     private HttpStatus status;
     private String message;

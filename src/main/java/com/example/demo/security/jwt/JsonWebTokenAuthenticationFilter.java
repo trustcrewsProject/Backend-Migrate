6efb27bda9b160cfe00fd6c16c432bd6e82f2a6b
songhyeonpk <ehftozl1997@gmail.com>
2023-11-29 16:53:32 +0900

    [feature/74-security-authentication] fix : attemptAuthentication() 로직의 예외처리 수정

--- src/main/java/com/example/demo/security/jwt/JsonWebTokenAuthenticationFilter.java
+++ src/main/java/com/example/demo/security/jwt/JsonWebTokenAuthenticationFilter.java
@@ -1,11 +1,13 @@
 package com.example.demo.security.jwt;
 
-
 import com.example.demo.dto.user.request.UserLoginRequestDto;
+import com.example.demo.global.exception.customexception.CommonCustomException;
+import com.example.demo.global.exception.customexception.UserCustomException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -28,20 +30,22 @@ public class JsonWebTokenAuthenticationFilter extends UsernamePasswordAuthentica
     // 로그인을 요청했을 때 실행되는 로직
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        UserLoginRequestDto loginRequest = null;
-
         try {
-            loginRequest = objectMapper.readValue(request.getInputStream(), UserLoginRequestDto.class);
-        } catch (Exception e) {
-            log.debug(e.getMessage());
+            ObjectMapper objectMapper = new ObjectMapper();
+            UserLoginRequestDto loginRequest = objectMapper.readValue(request.getInputStream(), UserLoginRequestDto.class);
+
+            // 토큰생성
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+            Authentication authentication = authenticationManager.authenticate(token);
+
+            return authentication;
+        } catch (IOException | NullPointerException e) {
+            log.error(e.getMessage());
+            throw CommonCustomException.INTERNAL_SERVER_ERROR;
+        } catch (AuthenticationException e) {
+            log.error(e.getMessage());
+            throw UserCustomException.INVALID_AUTHENTICATION;
         }
-
-        // 토큰생성
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
-        Authentication authentication = authenticationManager.authenticate(token);
-
-        return authentication;
     }
 
     // 인증에 성공했을 경우 실행되는 로직

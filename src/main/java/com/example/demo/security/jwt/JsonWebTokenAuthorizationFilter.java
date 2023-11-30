12579c82a5a6fa6718085581fcedd513388cb784
songhyeonpk <ehftozl1997@gmail.com>
2023-11-30 20:48:00 +0900

    [feature/74-security-jwt-authorization] feat : JWT 검증 및 인가처리를 담당하는 필터 생성

---
+++ src/main/java/com/example/demo/security/jwt/JsonWebTokenAuthorizationFilter.java
@@ -0,0 +1,19 @@
+package com.example.demo.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// JWT 검증하고 인가를 처리하는 필터
+@Slf4j
+public class JsonWebTokenAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+}

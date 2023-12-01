5e204ad45efa9c37aede0ccf1a1e4d12c6e18f9f
songhyeonpk <ehftozl1997@gmail.com>
2023-12-01 17:11:19 +0900

    [feature/25-user-signup] add : UserFacade 서비스에 회원 생성 로직에 필요한 의존성 추가

--- src/main/java/com/example/demo/service/user/UserFacade.java
+++ src/main/java/com/example/demo/service/user/UserFacade.java
@@ -1,9 +1,22 @@
 package com.example.demo.service.user;
 
+import com.example.demo.dto.common.ResponseDto;
+import com.example.demo.dto.user.request.UserCreateRequestDto;
+import com.example.demo.service.position.PositionService;
+import com.example.demo.service.technology_stack.TechnologyStackService;
+import com.example.demo.service.trust_score.TrustScoreService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserFacade {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PositionService positionService;
+    private final TechnologyStackService technologyStackService;
+    private final TrustScoreService trustScoreService;
 }

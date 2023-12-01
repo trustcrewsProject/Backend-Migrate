f99ffb3a5c07f4d3c1095bc5fc59905c2c883751
joo <rkdepik@gmail.com>
2023-12-01 21:29:26 +0900

    feature/76-add-validate-logic feat : 유효성 검증을 위한 상위분류 신뢰점수타입 조회 쿼리 개발

--- src/main/java/com/example/demo/repository/trust_score/TrustScoreTypeRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/trust_score/TrustScoreTypeRepositoryCustom.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository.trust_score;
 
+import java.util.List;
+
 public interface TrustScoreTypeRepositoryCustom {
     /**
      * 프로젝트가 아닌 경우 점수조회
-     *
      * @param trustScoreTypeId
      * @return
      */
@@ -11,9 +12,14 @@ public interface TrustScoreTypeRepositoryCustom {
 
     /**
      * 프로젝트에 해당하는 점수 조회
-     *
      * @param trueScoreTypeId
      * @return
      */
     int getScoreByProject(Long projectId, Long trueScoreTypeId);
+
+    /**
+     * 신뢰점수타입 대분류 아이디 조회
+     * @return List<Long>
+     */
+    List<Long> findAllUpScoreTypeId();
 }

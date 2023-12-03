ec36b102d5b6a67bcef7a1731d3c65fc97cc9c92
songhyeonpk <ehftozl1997@gmail.com>
2023-12-03 23:05:38 +0900

    [feature/25-user-signup] feat : UserFacade에 회원 생성 로직 구현

--- src/main/java/com/example/demo/service/user/UserFacade.java
+++ src/main/java/com/example/demo/service/user/UserFacade.java
@@ -1,7 +1,15 @@
 package com.example.demo.service.user;
 
+import com.example.demo.constant.Role;
+import com.example.demo.constant.TrustScoreTypeIdentifier;
 import com.example.demo.dto.common.ResponseDto;
+import com.example.demo.dto.trust_score.AddPointDto;
+import com.example.demo.dto.trust_score.request.TrustScoreUpdateRequestDto;
 import com.example.demo.dto.user.request.UserCreateRequestDto;
+import com.example.demo.model.position.Position;
+import com.example.demo.model.technology_stack.TechnologyStack;
+import com.example.demo.model.user.User;
+import com.example.demo.model.user.UserTechnologyStack;
 import com.example.demo.service.position.PositionService;
 import com.example.demo.service.technology_stack.TechnologyStackService;
 import com.example.demo.service.trust_score.TrustScoreService;
@@ -9,6 +17,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,4 +30,51 @@ public class UserFacade {
     private final PositionService positionService;
     private final TechnologyStackService technologyStackService;
     private final TrustScoreService trustScoreService;
+
+    // 회원가입
+    @Transactional
+    public ResponseDto<?> createUser(UserCreateRequestDto createRequest) {
+
+        // 패스워드 암호화
+        String encryptPassword = passwordEncoder.encode(createRequest.getPassword());
+
+        // 회원 포지션
+        Position position = positionService.findById(createRequest.getPositionId());
+
+        User user = User.builder()
+                .email(createRequest.getEmail())
+                .password(encryptPassword)
+                .nickname(createRequest.getNickname())
+                .profileImgSrc("")
+                .intro(createRequest.getIntro())
+                .position(position)
+                .role(Role.USER)
+                .build();
+
+        // 회원 기술스택 목록
+        List<UserTechnologyStack> techStacks = new ArrayList<>();
+        for(Long techStackId : createRequest.getTechStackIds()) {
+            TechnologyStack technologyStack = technologyStackService.findById(techStackId);
+            UserTechnologyStack userTechnologyStack = UserTechnologyStack.builder()
+                    .user(user)
+                    .technologyStack(technologyStack)
+                    .build();
+
+            techStacks.add(userTechnologyStack);
+        }
+        user.setTechStacks(techStacks);
+
+        // 회원 저장
+        User saveUser = userService.save(user);
+
+        // 신뢰점수 테이블에 회원 신뢰점수 저장
+        TrustScoreUpdateRequestDto trustScoreUpdateRequest = new TrustScoreUpdateRequestDto();
+        trustScoreUpdateRequest.setUserId(saveUser.getId());
+        trustScoreUpdateRequest.setScoreTypeId(TrustScoreTypeIdentifier.NEW_MEMBER);
+
+        AddPointDto addPoint = new AddPointDto(trustScoreUpdateRequest);
+        trustScoreService.addPoint(addPoint);
+
+        return ResponseDto.success("회원등록이 완료되었습니다.", saveUser.getId());
+    }
 }

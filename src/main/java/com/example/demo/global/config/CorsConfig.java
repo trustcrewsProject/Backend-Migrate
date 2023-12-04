6996124731bbb0b64fc72e6d366cccc68def48d5
songhyeonpk <ehftozl1997@gmail.com>
2023-12-04 22:43:59 +0900

    [feature/163-security-cors-config] feat : CorsConfig 추가

---
+++ src/main/java/com/example/demo/global/config/CorsConfig.java
@@ -0,0 +1,36 @@
+package com.example.demo.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+/**
+ *  CORS 설정 파일
+ */
+@Configuration
+public class CorsConfig {
+
+    // 배포 서버 주소
+    private static final String DEPLOYED_SERVER_ORIGIN = "*";
+
+    // 허용 HttpMethod 리스트
+    private static final List<String> PERMIT_HTTP_METHOD = List.of("GET", "POST", "PUT", "PATCH", "DELETE");
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin(DEPLOYED_SERVER_ORIGIN);
+        configuration.setAllowedMethods(PERMIT_HTTP_METHOD);
+        configuration.addAllowedHeader("*");
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+}

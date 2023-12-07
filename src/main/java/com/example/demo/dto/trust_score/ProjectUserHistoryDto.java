5c672e0143c2f51687d50c516148b6d6d3f69771
joo <rkdepik@gmail.com>
2023-12-07 15:44:31 +0900

    [feature/175-user-score-association] feat : 사용자와 신뢰점수 연관관계 연결 및 등급

--- src/main/java/com/example/demo/dto/trust_score/ProjectUserHistoryDto.java
+++ src/main/java/com/example/demo/dto/trust_score/ProjectUserHistoryDto.java
@@ -1,6 +1,8 @@
 package com.example.demo.dto.trust_score;
 
 import com.querydsl.core.annotations.QueryProjection;
+
+import java.time.LocalDateTime;
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,17 +12,17 @@ import lombok.Setter;
 public class ProjectUserHistoryDto {
     Long id;
     Boolean completeStatus;
-    String name;
+    String content;
     Integer trustScore;
-    Date date;
+    LocalDateTime createDate;
 
     @QueryProjection
     public ProjectUserHistoryDto(
-            Long workId, Integer score, Boolean completeStatus, String content, Date createDate) {
+            Long workId, Integer score, Boolean completeStatus, String content, LocalDateTime createDate) {
         this.id = workId;
         this.trustScore = score;
         this.completeStatus = completeStatus;
-        this.name = content;
-        this.date = createDate;
+        this.content = content;
+        this.createDate = createDate;
     }
 }

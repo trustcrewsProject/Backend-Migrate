8de019b154cd02d04453bbc25d5a007222303ef2
songhyeonpk <ehftozl1997@gmail.com>
2023-12-07 19:06:39 +0900

    [feature/163-querydsl-find-techstack-list] feat : 요청한 기술스택 PK 리스트로 기술스택 목록을 조회하는 쿼리 로직 구현

--- src/main/java/com/example/demo/repository/technology_stack/TechnologyStackRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/technology_stack/TechnologyStackRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.example.demo.repository.technology_stack;
 
+import com.example.demo.model.technology_stack.TechnologyStack;
+
+import java.util.List;
+
 public interface TechnologyStackRepositoryCustom {
+
+    // 요청한 기술스택 PK 리스트의 기술스택목록 조회
+    List<TechnologyStack> findTechnologyStackListByIds(List<Long> techStackIds);
 }
--- src/main/java/com/example/demo/repository/technology_stack/TechnologyStackRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/technology_stack/TechnologyStackRepositoryImpl.java
@@ -1,9 +1,15 @@
 package com.example.demo.repository.technology_stack;
 
+import com.example.demo.global.exception.customexception.TechnologyStackCustomException;
+import com.example.demo.model.technology_stack.QTechnologyStack;
+import com.example.demo.model.technology_stack.TechnologyStack;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -11,4 +17,37 @@ import org.springframework.stereotype.Repository;
 public class TechnologyStackRepositoryImpl implements TechnologyStackRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<TechnologyStack> findTechnologyStackListByIds(List<Long> techStackIds) {
+        QTechnologyStack qTechnologyStack = QTechnologyStack.technologyStack;
+
+        List<TechnologyStack> technologyStackList = jpaQueryFactory
+                .selectFrom(qTechnologyStack)
+                .where(qTechnologyStack.id.in(techStackIds))
+                .fetch();
+
+        // 검증로직
+        validateTechnologyStackExistence(techStackIds, technologyStackList);
+
+        return technologyStackList;
+    }
+
+    // 요청한 기술스택 ID 목록과 조회한 기술스택 목록이 동일한지 검증
+    private void validateTechnologyStackExistence(List<Long> techStackIds, List<TechnologyStack> foundTechnologyStacks) {
+        List<Long> foundIds = foundTechnologyStacks.stream()
+                .map(TechnologyStack::getId)
+                .collect(Collectors.toList());
+
+        List<Long> notFoundIds = techStackIds.stream()
+                .filter(id -> !foundIds.contains(id))
+                .collect(Collectors.toList());
+
+
+        if(!notFoundIds.isEmpty()) {
+            log.error("NOT_FOUND_ID : {}", notFoundIds);
+            throw TechnologyStackCustomException.NOT_FOUND_TECHNOLOGY_STACK;
+        }
+    }
+
 }

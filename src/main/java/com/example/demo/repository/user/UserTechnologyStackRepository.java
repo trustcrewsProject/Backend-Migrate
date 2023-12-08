94e92d977aec4bbd94e375ae465050b3cf6ad6e2
songhyeonpk <ehftozl1997@gmail.com>
2023-12-08 14:08:54 +0900

    [feature/163-user-techstack-list-save] feat : JpaRepository를 확장한 UserTechnologyStackRepository 생성

---
+++ src/main/java/com/example/demo/repository/user/UserTechnologyStackRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository.user;
+
+import com.example.demo.model.user.UserTechnologyStack;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserTechnologyStackRepository extends JpaRepository<UserTechnologyStack, Long> {
+}

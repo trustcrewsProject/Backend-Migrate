43b980d9a208fd9059079c3870995b77175b4c1a
songhyeonpk <ehftozl1997@gmail.com>
2023-12-08 16:41:28 +0900

    [feature/163-fix-user-signup] refactor : 회원가입 로직 코드 리펙토링

--- src/main/java/com/example/demo/service/user/UserFacade.java
+++ src/main/java/com/example/demo/service/user/UserFacade.java
@@ -9,6 +9,7 @@ import com.example.demo.dto.user.request.UserCreateRequestDto;
 import com.example.demo.global.exception.customexception.TrustScoreCustomException;
 import com.example.demo.model.position.Position;
 import com.example.demo.model.technology_stack.TechnologyStack;
+import com.example.demo.model.trust_score.TrustScore;
 import com.example.demo.model.user.User;
 import com.example.demo.model.user.UserTechnologyStack;
 import com.example.demo.repository.trust_score.TrustScoreRepository;
@@ -38,13 +39,13 @@ public class UserFacade {
     // 회원가입
     @Transactional
     public ResponseDto<?> createUser(UserCreateRequestDto createRequest) {
-
         // 패스워드 암호화
         String encryptPassword = passwordEncoder.encode(createRequest.getPassword());
 
-        // 회원 포지션
+        // 회원 포지션 조회
         Position position = positionService.findById(createRequest.getPositionId());
 
+        // 회원 엔티티 생성
         User user =
                 User.builder()
                         .email(createRequest.getEmail())
@@ -59,44 +60,57 @@ public class UserFacade {
         // 회원 저장
         User saveUser = userService.save(user);
 
-        // 신뢰점수 테이블에 회원 신뢰점수 저장
-        TrustScoreUpdateRequestDto trustScoreUpdateRequest = new TrustScoreUpdateRequestDto();
-        trustScoreUpdateRequest.setUserId(saveUser.getId());
-        trustScoreUpdateRequest.setScoreTypeId(TrustScoreTypeIdentifier.NEW_MEMBER);
-
-        AddPointDto addPoint = new AddPointDto(trustScoreUpdateRequest);
-        trustScoreService.addPoint(addPoint);
+        // 신뢰점수 저장
+        TrustScore trustScore = addInitialTrustScoreReturnResponse(saveUser);
 
         // 회원에 신뢰점수 세팅
-        saveUser.setTrustScore(trustScoreService.findTrustScoreByUserId(user.getId()));
-
-        // 요청한 기술스택 목록 조회
-        List<TechnologyStack> techStackList = technologyStackService.findTechnologyStackListByIds(createRequest.getTechStackIds());
+        saveUser.setTrustScore(trustScore);
 
         // 회원 기술스택 목록 저장
-        List<UserTechnologyStack> userTechStackList = createAndSaveUserTechnologyStacks(saveUser, techStackList);
+        List<UserTechnologyStack> userTechnologyStackList = saveTechStacksAndReturnResponse(saveUser, createRequest.getTechStackIds());
 
         // 회원 기술스택 목록 세팅
-        saveUser.setTechStacks(userTechStackList);
+        saveUser.setTechStacks(userTechnologyStackList);
 
         return ResponseDto.success("회원등록이 완료되었습니다.", saveUser.getId());
     }
 
-    // UserTechnologyStack 생성 및 저장
-    private List<UserTechnologyStack> createAndSaveUserTechnologyStacks(User user, List<TechnologyStack> techStackList) {
-        List<UserTechnologyStack> userTechnologyStackList = new ArrayList<>();
+    // 신뢰점수 저장 및 반환
+    private TrustScore addInitialTrustScoreReturnResponse(User user) {
+        TrustScoreUpdateRequestDto trustScoreUpdateRequest = new TrustScoreUpdateRequestDto();
+        trustScoreUpdateRequest.setUserId(user.getId());
+        trustScoreUpdateRequest.setScoreTypeId(TrustScoreTypeIdentifier.NEW_MEMBER);
 
-        // 회원 엔티티와 기술스택 목록으로 UserTechnologyStack 엔티티 생성
-        for(TechnologyStack technologyStack : techStackList) {
-            UserTechnologyStack userTechnologyStack = UserTechnologyStack.builder()
-                    .user(user)
-                    .technologyStack(technologyStack)
-                    .build();
+        // 회원의 신뢰점수 저장
+        AddPointDto addPoint = new AddPointDto(trustScoreUpdateRequest);
+        trustScoreService.addPoint(addPoint);
 
-            userTechnologyStackList.add(userTechnologyStack);
-        }
+        // 해당 회원의 신뢰점수 반환
+        return trustScoreService.findTrustScoreByUserId(user.getId());
+    }
 
-        // UserTechnologyStack 저장하고 리스트로 반환
-        return userTechnologyStackService.saveAll(userTechnologyStackList);
+    // 회원 기술스택 목록 저장 및 반환
+    private List<UserTechnologyStack> saveTechStacksAndReturnResponse(User user, List<Long> techStackIds) {
+        // 요청한 기술스택 목록 조회
+        List<TechnologyStack> techStackList = technologyStackService.findTechnologyStackListByIds(techStackIds);
+        List<UserTechnologyStack> userTechStackList = createUserTechnologyStackList(user, techStackList);
+
+        // 회원 기술스택 목록 저장
+        return userTechnologyStackService.saveAll(userTechStackList);
+    }
+
+    // 회원 기술스택 목록 생성
+    private List<UserTechnologyStack> createUserTechnologyStackList(User user, List<TechnologyStack> techStackList) {
+        return techStackList.stream()
+                .map(tech -> createUserTechnologyStack(user, tech))
+                .collect(Collectors.toList());
+    }
+
+    // 회원 기술스택 엔티티 생성
+    private UserTechnologyStack createUserTechnologyStack(User user, TechnologyStack technologyStack) {
+        return UserTechnologyStack.builder()
+                .user(user)
+                .technologyStack(technologyStack)
+                .build();
     }
 }

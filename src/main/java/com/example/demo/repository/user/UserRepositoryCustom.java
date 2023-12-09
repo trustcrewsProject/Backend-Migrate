6c05754f753917fc53848cc5927f9591864edb8a
songhyeonpk <ehftozl1997@gmail.com>
2023-12-09 14:00:06 +0900

    [feature/163-user-update] feat : 회원 수정 요청 시 회원 상세정보 조회 queryDsl 구현

--- src/main/java/com/example/demo/repository/user/UserRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/user/UserRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.example.demo.repository.user;
 
+import com.example.demo.model.user.User;
+
 public interface UserRepositoryCustom {
+
+    // 회원 업데이트를 위한 사용자 세부 정보 가져오기(포지션, 회원 기술스택, 기술스택)
+    User fetchUserDetailsForUpdate(Long userId);
 }

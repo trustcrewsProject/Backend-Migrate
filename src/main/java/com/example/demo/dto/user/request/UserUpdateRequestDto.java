ea19bf34aacb7ba3006049807ddd8be3339c2246
songhyeonpk <ehftozl1997@gmail.com>
2023-12-09 13:53:58 +0900

    [feature/163-user-update] fix : UserUpdateRequestDto에 유효성 검사 코드 추가

--- src/main/java/com/example/demo/dto/user/request/UserUpdateRequestDto.java
+++ src/main/java/com/example/demo/dto/user/request/UserUpdateRequestDto.java
@@ -4,14 +4,23 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 @Getter
 @AllArgsConstructor
 public class UserUpdateRequestDto {
 
+    @NotBlank(message = "닉네임은 필수 입력 값입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{6,10}$", message = "닉네임은 영문과 숫자를 조합하여 6자~10자 사이로 설정해주세요.")
     private String nickname;
 
+    @NotNull(message = "직무는 필수 선택 값입니다.")
     private Long positionId;
 
+    @NotEmpty(message = "관심스택은 필수 선택 값입니다.")
     private List<Long> techStackIds;
 
     private String intro;

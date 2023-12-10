d5cfb0d0387c6d8594dac825d8772252419d81d2
jkang1201 <rkdepik@gmail.com>
2023-12-10 09:42:11 +0900

    [feat/175-score-type-info] test : 신뢰점수타입 조회 테스트 구현

--- src/test/java/com/example/demo/trust_score/service/TrustScoreTypeServiceTest.java
+++ src/test/java/com/example/demo/trust_score/service/TrustScoreTypeServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.trust_score.service;
 
 import com.example.demo.dto.trust_score_type.response.TrustScoreTypeReadResponseDto;
+import com.example.demo.model.trust_score.TrustScoreType;
+import com.example.demo.repository.trust_score.TrustScoreTypeRepository;
 import com.example.demo.service.trust_score.TrustScoreTypeService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,8 +20,11 @@ public class TrustScoreTypeServiceTest {
     @Autowired
     private TrustScoreTypeService trustScoreTypeService;
 
+    @Autowired
+    private TrustScoreTypeRepository trustScoreTypeRepository;
+
     @Test
-    @DisplayName("신뢰점수타입 전체 DTO로 변환")
+    @DisplayName("신뢰점수타입 전체 DTO 반환")
     public void getAllAndReturnDto_Method_Test_Pass() {
 
         // when
@@ -28,5 +33,29 @@ public class TrustScoreTypeServiceTest {
 
         // then
         Assertions.assertThat(getAllAndReturnDto.size()).isEqualTo(26);
+    }
+
+    @Test
+    @DisplayName("개별 신뢰점수타입 DTO 반환")
+    public void findByIdAndReturnDto_Method_Test_Pass() {
+        // given
+        TrustScoreType trustScoreType = TrustScoreType.builder()
+                            .upTrustScoreType(null)
+                            .trustScoreTypeName("테스트 신뢰정보타입")
+                            .trustGradeName("테스트 신뢰등급")
+                            .gubunCode("테스트 구분코드")
+                            .score(77777)
+                            .deleteStatus("N")
+                            .build();
+
+
+        // when
+
+        TrustScoreType saveTrustScoreType = trustScoreTypeRepository.save(trustScoreType);
+        TrustScoreTypeReadResponseDto responseDto = trustScoreTypeService.findByIdAndReturnDto(saveTrustScoreType.getId());
+
+        // then
+        Assertions.assertThat(responseDto.getTrustScoreTypeId()).isGreaterThan(26);
+        Assertions.assertThat(responseDto.getTrustScoreTypeName()).isEqualTo("테스트 신뢰정보타입");
     }
 }

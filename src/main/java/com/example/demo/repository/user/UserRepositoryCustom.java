8d930c28ce5a8b205d6aecceb217d2f46770bc98
songhyeonpk <ehftozl1997@gmail.com>
2023-12-11 22:50:59 +0900

    [feature/163-user-my-info] feat : UserRepository 회원의 모든 정보를 조회하는 쿼리 로직 추가

--- src/main/java/com/example/demo/repository/user/UserRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/user/UserRepositoryCustom.java
@@ -6,4 +6,7 @@ public interface UserRepositoryCustom {
 
     // 회원 업데이트를 위한 사용자 세부 정보 가져오기(포지션, 회원 기술스택, 기술스택)
     User fetchUserDetailsForUpdate(Long userId);
+
+    // 회원 모든 정보 가져오기 (포지션, 회원 기술스택, 기술스택, 신뢰점수, 신뢰등급)
+    User fetchUserByUserIdWithAllAttributes(Long userId);
 }
--- src/main/java/com/example/demo/repository/user/UserRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/user/UserRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.example.demo.repository.user;
 
+import com.example.demo.model.position.QPosition;
 import com.example.demo.model.technology_stack.QTechnologyStack;
+import com.example.demo.model.trust_grade.QTrustGrade;
+import com.example.demo.model.trust_score.QTrustScore;
 import com.example.demo.model.user.QUser;
 import com.example.demo.model.user.QUserTechnologyStack;
 import com.example.demo.model.user.User;
@@ -26,6 +29,26 @@ public class UserRepositoryImpl implements UserRepositoryCustom {
                 .fetchJoin()
                 .leftJoin(qUserTechnologyStack.technologyStack, qTechnologyStack)
                 .fetchJoin()
+                .where(qUser.id.eq(userId))
+                .fetchOne();
+    }
+
+    @Override
+    public User fetchUserByUserIdWithAllAttributes(Long userId) {
+        QUser qUser = QUser.user;
+        QPosition qPosition = QPosition.position;
+        QUserTechnologyStack qUserTechnologyStack = QUserTechnologyStack.userTechnologyStack;
+        QTechnologyStack qTechnologyStack = QTechnologyStack.technologyStack;
+        QTrustScore qTrustScore = QTrustScore.trustScore;
+        QTrustGrade qTrustGrade = QTrustGrade.trustGrade;
+
+        return jpaQueryFactory
+                .selectFrom(qUser)
+                .leftJoin(qUser.position, qPosition).fetchJoin()
+                .leftJoin(qUser.techStacks, qUserTechnologyStack).fetchJoin()
+                .leftJoin(qUserTechnologyStack.technologyStack, qTechnologyStack).fetchJoin()
+                .leftJoin(qUser.trustScore, qTrustScore).fetchJoin()
+                .leftJoin(qTrustScore.trustGrade, qTrustGrade).fetchJoin()
                 .where(qUser.id.eq(userId))
                 .fetchOne();
     }

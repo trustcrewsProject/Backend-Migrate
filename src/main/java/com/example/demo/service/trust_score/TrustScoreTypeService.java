606fd72740c093e0ef8248a7a2457464c8486f32
joo <rkdepik@gmail.com>
2023-12-11 21:34:13 +0900

    [feat/175-type-update] feat : 신뢰점수타입 업데이트 Service 메서드 구현

--- src/main/java/com/example/demo/service/trust_score/TrustScoreTypeService.java
+++ src/main/java/com/example/demo/service/trust_score/TrustScoreTypeService.java
@@ -2,6 +2,7 @@ package com.example.demo.service.trust_score;
 
 import com.example.demo.dto.trust_score_type.TrustScoreTypeSearchCriteria;
 import com.example.demo.dto.trust_score_type.request.TrustScoreTypeCreateRequestDto;
+import com.example.demo.dto.trust_score_type.request.TrustScoreTypeUpdateRequestDto;
 import com.example.demo.dto.trust_score_type.response.TrustScoreTypeCreateResponseDto;
 import com.example.demo.dto.trust_score_type.response.TrustScoreTypeReadResponseDto;
 
@@ -13,4 +14,5 @@ public interface TrustScoreTypeService {
             TrustScoreTypeSearchCriteria criteria);
     TrustScoreTypeReadResponseDto findByIdAndReturnDto(Long trustScoreTypeId);
     TrustScoreTypeCreateResponseDto createTrustScoreType(TrustScoreTypeCreateRequestDto requestDto);
+    void updateTrustScoreType(Long trustScoreTypeId, TrustScoreTypeUpdateRequestDto requestDto);
 }

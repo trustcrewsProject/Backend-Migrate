5e5ce37f17e07b25fac0a5f135a071309838aa96
songhyeonpk <ehftozl1997@gmail.com>
2023-12-13 14:06:54 +0900

    [feature/220-token-reissue] feat : 회원 토큰 재발급 요청 DTO 생성

---
+++ src/main/java/com/example/demo/dto/user/request/UserTokenReissueRequestDto.java
@@ -0,0 +1,13 @@
+package com.example.demo.dto.user.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserTokenReissueRequestDto {
+
+    private Long userId;
+}

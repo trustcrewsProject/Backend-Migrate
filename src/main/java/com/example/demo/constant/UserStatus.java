5d7ef78e31d513a148ddc360f6bc30c4358d831c
songhyeonpk <ehftozl1997@gmail.com>
2023-12-13 15:40:55 +0900

    [feature/add-user-status-enum] feat : 회원 상태를 관리하는 UserStatus enum 생성

---
+++ src/main/java/com/example/demo/constant/UserStatus.java
@@ -0,0 +1,12 @@
+package com.example.demo.constant;
+
+/**
+ *  회원 상태 ENUM
+ *  ACTIVE(활동중인 회원), SUSPENDED(휴먼상태인 회원), DELETED(탈퇴한 회원)으로 구분
+ */
+public enum UserStatus {
+
+    ACTIVE,
+    SUSPENDED,
+    DELETED;
+}

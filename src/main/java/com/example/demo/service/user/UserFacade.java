4cf6e0f2ef10ff83be0e10b9ac59f2cab1d9e0f5
songhyeonpk <ehftozl1997@gmail.com>
2023-12-15 17:13:30 +0900

    [feature/fix-get-myinfo] fix : UserFacade 내 정보 조회 로직에서 UserMyInfoResponseDto 빌드 코드 수정

--- src/main/java/com/example/demo/service/user/UserFacade.java
+++ src/main/java/com/example/demo/service/user/UserFacade.java
@@ -246,7 +246,7 @@ public class UserFacade {
 
         // 내 정보 응답 DTO 생성
         UserMyInfoResponseDto myInfoResponse = UserMyInfoResponseDto.of(currentUser.getId(), currentUser.getEmail(), currentUser.getNickname(),
-                currentUser.getProfileImgSrc(), trustScore.getScore(), trustGradeInfo, positionInfo, techStacksInfo, projectHistoryTotalCount,
+                currentUser.getProfileImgSrc(), currentUser.getIntro(), trustScore.getScore(), trustGradeInfo, positionInfo, techStacksInfo, projectHistoryTotalCount,
                 DateTimeConverter.toStringConvert(currentUser.getCreateDate()), DateTimeConverter.toStringConvert(currentUser.getUpdateDate()));
 
         return ResponseDto.success("내 정보 조회가 완료되었습니다.", myInfoResponse);

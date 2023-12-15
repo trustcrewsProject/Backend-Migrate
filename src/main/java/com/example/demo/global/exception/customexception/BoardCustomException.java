64e64de381d95556b5a0085eb198bfb614a6a29e
songhyeonpk <ehftozl1997@gmail.com>
2023-12-15 16:52:55 +0900

    [feature/fix-update-board] feat : 게시글 조작 권한 관련 에러코드 및 커스텀 예외 추가

--- src/main/java/com/example/demo/global/exception/customexception/BoardCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/BoardCustomException.java
@@ -7,6 +7,9 @@ public class BoardCustomException extends CustomException {
     public static final BoardCustomException NOT_FOUND_BOARD =
             new BoardCustomException(BoardErrorCode.NOT_FOUND_BOARD);
 
+    public static final BoardCustomException NO_PERMISSION_TO_EDIT_OR_DELETE =
+            new BoardCustomException(BoardErrorCode.NO_PERMISSION_TO_EDIT_OR_DELETE);
+
     public BoardCustomException(BoardErrorCode boardErrorCode) {
         super(boardErrorCode);
     }
--- src/main/java/com/example/demo/global/exception/errorcode/BoardErrorCode.java
+++ src/main/java/com/example/demo/global/exception/errorcode/BoardErrorCode.java
@@ -5,7 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum BoardErrorCode implements ErrorCode {
-    NOT_FOUND_BOARD(HttpStatus.NOT_FOUND, "해당 게시글이 존재하지 않습니다.");
+    NOT_FOUND_BOARD(HttpStatus.NOT_FOUND, "해당 게시글이 존재하지 않습니다."),
+    NO_PERMISSION_TO_EDIT_OR_DELETE(HttpStatus.FORBIDDEN, "해당 게시글을 수정 및 삭제할 권한이 존재하지 않습니다.");
 
     private HttpStatus status;
     private String message;

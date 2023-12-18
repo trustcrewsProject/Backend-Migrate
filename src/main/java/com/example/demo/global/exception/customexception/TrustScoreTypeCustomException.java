14ddeffb52783497ac266f1ddfde22eac514bf44
joo <rkdepik@gmail.com>
2023-12-18 19:50:40 +0900

    [feat/175-validate-type] feat : 신뢰점수타입 비활성화 유효성 검사 구현

--- src/main/java/com/example/demo/global/exception/customexception/TrustScoreTypeCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/TrustScoreTypeCustomException.java
@@ -7,6 +7,9 @@ public class TrustScoreTypeCustomException extends CustomException {
     public static final TrustScoreTypeCustomException NOT_FOUND_TRUST_SCORE_TYPE =
             new TrustScoreTypeCustomException(TrustScoreTypeErrorCode.NOT_FOUND_TRUST_SCORE_TYPE);
 
+    public static final TrustScoreTypeCustomException ALREADY_DISABLED_TRUST_SCORE_TYPE =
+            new TrustScoreTypeCustomException(TrustScoreTypeErrorCode.ALREADY_DISABLED_TRUST_SCORE_TYPE);
+
     public TrustScoreTypeCustomException(TrustScoreTypeErrorCode trustScoreTypeErrorCode) {
         super(trustScoreTypeErrorCode);
     }

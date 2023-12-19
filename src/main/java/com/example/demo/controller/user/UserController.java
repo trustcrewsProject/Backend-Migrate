c12b0b9fbf57db90d627ad674cc8a58a57b37dc8
songhyeonpk <ehftozl1997@gmail.com>
2023-12-19 18:55:14 +0900

    [feature/refactor-user-controller] refactor : UserController URI 매핑 수정

--- src/main/java/com/example/demo/controller/user/UserController.java
+++ src/main/java/com/example/demo/controller/user/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -21,26 +22,26 @@ public class UserController {
     private final UserService userService;
 
     // 이메일 중복확인 요청
-    @GetMapping("/api/user/check-email/{email}")
+    @GetMapping("/check-email/{email}")
     public ResponseEntity<ResponseDto<?>> checkEmail(@PathVariable String email) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.checkEmail(email));
     }
 
     // 닉네임 중복확인 요청
-    @GetMapping("/api/user/check-nickname/{nickname}")
+    @GetMapping("/check-nickname/{nickname}")
     public ResponseEntity<ResponseDto<?>> checkNickname(@PathVariable String nickname) {
         return ResponseEntity.status(HttpStatus.OK).body(userService.checkNickname(nickname));
     }
 
     // 회원가입
-    @PostMapping("/api/user")
+    @PostMapping()
     public ResponseEntity<ResponseDto<?>> signup(
             @Valid @RequestBody UserCreateRequestDto createRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userFacade.createUser(createRequest));
     }
 
     // 회원수정
-    @PutMapping("/api/user")
+    @PutMapping()
     public ResponseEntity<ResponseDto<?>> update(
             @AuthenticationPrincipal PrincipalDetails user,
             @Valid @RequestBody UserUpdateRequestDto updateRequest) {
@@ -49,19 +50,19 @@ public class UserController {
     }
 
     // 간단한 내 정보 조회
-    @GetMapping("/api/user/simple-me")
+    @GetMapping("/simple-me")
     public ResponseEntity<ResponseDto<?>> simpleMyInfo(@AuthenticationPrincipal PrincipalDetails user) {
         return ResponseEntity.status(HttpStatus.OK).body(userFacade.getSimpleMyInfo(user));
     }
 
     // 내 정보 조회
-    @GetMapping("/api/user/me")
+    @GetMapping("/me")
     public ResponseEntity<ResponseDto<?>> myInfo(@AuthenticationPrincipal PrincipalDetails user) {
         return ResponseEntity.status(HttpStatus.OK).body(userFacade.getMyInfo(user));
     }
 
     // 내 프로젝트 이력 목록 조회
-    @GetMapping("/api/user/me/project-history")
+    @GetMapping("/me/project-history")
     public ResponseEntity<ResponseDto<?>> myProjectHistoryList(@AuthenticationPrincipal PrincipalDetails user, @RequestParam int pageNumber) {
         return ResponseEntity.status(HttpStatus.OK).body(userFacade.getMyProjectHistoryList(user, pageNumber));
     }

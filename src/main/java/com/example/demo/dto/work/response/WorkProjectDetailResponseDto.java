c30fb9023c6dd6da4acf2151f3d62387b5d75ef2
songhyeonpk <ehftozl1997@gmail.com>
2023-12-20 20:46:11 +0900

    [feature/240-response-dto-localdatetime-serialization] add : 업무 관련 응답 dto에 LocalDateTime 타입 직렬화 코드 추가

--- src/main/java/com/example/demo/dto/work/response/WorkProjectDetailResponseDto.java
+++ src/main/java/com/example/demo/dto/work/response/WorkProjectDetailResponseDto.java
@@ -1,11 +1,14 @@
 package com.example.demo.dto.work.response;
 
 import com.example.demo.dto.user.response.UserProjectDetailResponseDto;
+import com.example.demo.global.util.LocalDateTimeFormatSerializer;
 import com.example.demo.model.project.ProjectMember;
 import com.example.demo.model.work.Work;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -20,7 +23,11 @@ public class WorkProjectDetailResponseDto {
     private Boolean completeStatus;
     private LocalDate startDate;
     private LocalDate endDate;
+
+    @JsonSerialize(using = LocalDateTimeFormatSerializer.class)
     private LocalDateTime createDate;
+
+    @JsonSerialize(using = LocalDateTimeFormatSerializer.class)
     private LocalDateTime updateDate;
 
     public static WorkProjectDetailResponseDto of(

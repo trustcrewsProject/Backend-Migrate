8a9e8e66b411d854fe581f8d1242317fa7adffa8
songhyeonpk <ehftozl1997@gmail.com>
2023-12-20 20:45:15 +0900

    [feature/240-response-dto-localdatetime-serialization] add : 포지션 멤버 관련 응답 dto에 LocalDateTime 타입 직렬화 코드 추가

--- src/main/java/com/example/demo/dto/projectmember/response/ProjectMemberDetailResponseDto.java
+++ src/main/java/com/example/demo/dto/projectmember/response/ProjectMemberDetailResponseDto.java
@@ -3,8 +3,11 @@ package com.example.demo.dto.projectmember.response;
 import com.example.demo.constant.ProjectMemberStatus;
 import com.example.demo.dto.position.response.PositionResponseDto;
 import com.example.demo.dto.user.response.UserProjectDetailResponseDto;
+import com.example.demo.global.util.LocalDateTimeFormatSerializer;
 import com.example.demo.model.project.ProjectMember;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -16,7 +19,11 @@ public class ProjectMemberDetailResponseDto {
     private ProjectMemberAuthResponseDto projectMemberAuth;
     private PositionResponseDto position;
     private ProjectMemberStatus status;
+
+    @JsonSerialize(using = LocalDateTimeFormatSerializer.class)
     private LocalDateTime createDate;
+
+    @JsonSerialize(using = LocalDateTimeFormatSerializer.class)
     private LocalDateTime updateDate;
 
     public static ProjectMemberDetailResponseDto of(
--- src/main/java/com/example/demo/dto/projectmember/response/ProjectMemberReadProjectCrewsResponseDto.java
+++ src/main/java/com/example/demo/dto/projectmember/response/ProjectMemberReadProjectCrewsResponseDto.java
@@ -2,8 +2,11 @@ package com.example.demo.dto.projectmember.response;
 
 import com.example.demo.dto.position.response.PositionResponseDto;
 import com.example.demo.dto.user.response.UserReadProjectCrewResponseDto;
+import com.example.demo.global.util.LocalDateTimeFormatSerializer;
 import com.example.demo.model.project.ProjectMember;
 import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -14,6 +17,8 @@ public class ProjectMemberReadProjectCrewsResponseDto {
     private UserReadProjectCrewResponseDto user;
     private ProjectMemberAuthResponseDto projectMemberAuth;
     private PositionResponseDto position;
+
+    @JsonSerialize(using = LocalDateTimeFormatSerializer.class)
     private LocalDateTime lastWorkDate;
 
     public static ProjectMemberReadProjectCrewsResponseDto of(

31ade135ac2ca21dce77ffabe54204338fc6f735
songhyeonpk <ehftozl1997@gmail.com>
2023-12-21 14:28:25 +0900

    [feature/240-custom-access-denied-handler] feat : 시큐리티 필터 단에서 접근 권한 관련 예외를 처리하기 위한 CustomAccessDeniedHandler 추가

---
+++ src/main/java/com/example/demo/security/custom/CustomAccessDeniedHandler.java
@@ -0,0 +1,30 @@
+package com.example.demo.security.custom;
+
+import com.example.demo.dto.common.ResponseDto;
+import com.example.demo.global.exception.customexception.UserCustomException;
+import com.example.demo.global.exception.errorcode.UserErrorCode;
+import com.example.demo.security.SecurityResponseHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *  시큐리티 필터단에서 접근 요청한 회원의 권한이 없거나 부족한 경우 예외를 처리하는 핸들러
+ */
+@RequiredArgsConstructor
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final UserErrorCode ACCESS_DENIED = UserErrorCode.ACCESS_DENIED;
+    private final SecurityResponseHandler securityResponseHandler;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        securityResponseHandler.sendResponse(response, ACCESS_DENIED.getStatus(), ResponseDto.fail(ACCESS_DENIED.getMessage()));
+    }
+}

cf977763de9509e4e1533826baa2c6996281d4e5
songhyeonpk <ehftozl1997@gmail.com>
2023-12-22 16:53:27 +0900

    [feature/fix-board-search] fix : BoardRepositoryCustom의 게시글 검색 목록 조회 쿼리 파라미터 수정

--- src/main/java/com/example/demo/repository/board/BoardRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/board/BoardRepositoryCustom.java
@@ -5,8 +5,10 @@ import com.example.demo.dto.board.response.BoardSearchResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 public interface BoardRepositoryCustom {
 
     Page<BoardSearchResponseDto> getBoardSearchPage(
-            BoardSearchRequestDto boardSearchRequestDto, Pageable pageable);
+            Long positionId, String keyword, List<Long> technologyIds, Pageable pageable);
 }

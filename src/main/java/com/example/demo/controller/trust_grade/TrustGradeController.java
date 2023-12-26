427c0aa1f1019eb8891fc71ec9f25b429b28ba9f
jkang1201 <rkdepik@gmail.com>
2023-12-26 20:34:31 +0900

    feat : 신뢰등급 조회 Controller 구현

--- src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
+++ src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
@@ -1,6 +1,21 @@
 package com.example.demo.controller.trust_grade;
 
+import com.example.demo.dto.trust_grade.response.TrustGradeInfoResponseDto;
+import com.example.demo.service.trust_grade.TrustGradeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-public class TrustGradeController {}
+@RequiredArgsConstructor
+public class TrustGradeController {
+
+    private final TrustGradeService trustGradeService;
+
+    @GetMapping("/api/trust-grade")
+    public List<TrustGradeInfoResponseDto> getListByCriteria() {
+        return trustGradeService.getListByCriteria();
+    }
+}

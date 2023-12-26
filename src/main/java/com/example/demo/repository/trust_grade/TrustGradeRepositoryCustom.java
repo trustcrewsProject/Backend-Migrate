c5f1e557e132474f6ecf66ac517c370f8a27b2d8
jkang1201 <rkdepik@gmail.com>
2023-12-26 20:30:14 +0900

    feat : 신뢰등급 조회 Repository 구현

---
+++ src/main/java/com/example/demo/repository/trust_grade/TrustGradeRepositoryCustom.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository.trust_grade;
+
+import com.example.demo.dto.trust_grade.response.TrustGradeInfoResponseDto;
+import com.querydsl.core.types.dsl.BooleanExpression;
+
+import java.util.List;
+
+public interface TrustGradeRepositoryCustom {
+
+    List<TrustGradeInfoResponseDto> getListByCriteria(BooleanExpression builder);
+}

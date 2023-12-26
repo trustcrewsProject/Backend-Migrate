11052dfa96989c94eafc189d81fc2dd82c7bbcef
jkang1201 <rkdepik@gmail.com>
2023-12-26 20:50:33 +0900

    fix : Controller 컨벤션에 맞게 수정

--- src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
+++ src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
@@ -1,8 +1,11 @@
 package com.example.demo.controller.trust_grade;
 
+import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.dto.trust_grade.response.TrustGradeInfoResponseDto;
 import com.example.demo.service.trust_grade.TrustGradeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,8 @@ public class TrustGradeController {
     private final TrustGradeService trustGradeService;
 
     @GetMapping("/api/trust-grade")
-    public List<TrustGradeInfoResponseDto> getListByCriteria() {
-        return trustGradeService.getListByCriteria();
+    public ResponseEntity<?> getListByCriteria() {
+        List<TrustGradeInfoResponseDto> result = trustGradeService.getListByCriteria();
+        return new ResponseEntity<>(ResponseDto.success("success", result), HttpStatus.OK);
     }
 }

1de784d46d15057aa7882bf7f0fdc2d387f995a7
jkang1201 <rkdepik@gmail.com>
2024-01-01 21:02:46 +0900

    feat : 회원 지원 가능 등급 조회 Service 구현

--- src/main/java/com/example/demo/service/trust_grade/TrustGradeServiceImpl.java
+++ src/main/java/com/example/demo/service/trust_grade/TrustGradeServiceImpl.java
@@ -2,9 +2,13 @@ package com.example.demo.service.trust_grade;
 
 import com.example.demo.dto.trust_grade.response.TrustGradeInfoResponseDto;
 import com.example.demo.global.exception.customexception.TrustGradeCustomException;
+import com.example.demo.global.exception.customexception.UserCustomException;
 import com.example.demo.model.trust_grade.TrustGrade;
+import com.example.demo.model.user.User;
 import com.example.demo.repository.trust_grade.TrustGradeRepository;
 import java.util.List;
+
+import com.example.demo.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +16,7 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class TrustGradeServiceImpl implements TrustGradeService {
     private final TrustGradeRepository trustGradeRepository;
+    private final UserRepository userRepository;
 
     @Override
     public TrustGrade getTrustGradeById(Long id) {
@@ -23,5 +28,12 @@ public class TrustGradeServiceImpl implements TrustGradeService {
     @Override
     public List<TrustGradeInfoResponseDto> getListByCriteria() {
         return trustGradeRepository.getListByCriteria(null);
+    }
+
+    @Override
+    public List<TrustGradeInfoResponseDto> getPossibleUserGrades(Long userId) {
+        User findUser = userRepository.findById(userId)
+                .orElseThrow(() -> UserCustomException.NOT_FOUND_USER);
+        return trustGradeRepository.getPossibleTrustGrades(findUser.getTrustScore().getScore());
     }
 }

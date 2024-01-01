71c5ae1aafb2304bcf62de5976b1a92d1cedd440
jkang1201 <rkdepik@gmail.com>
2024-01-01 21:02:52 +0900

    feat : 회원 지원 가능 등급 조회 Controller 구현

--- src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
+++ src/main/java/com/example/demo/controller/trust_grade/TrustGradeController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,6 +20,12 @@ public class TrustGradeController {
     @GetMapping("/api/trust-grade")
     public ResponseEntity<?> getListByCriteria() {
         List<TrustGradeInfoResponseDto> result = trustGradeService.getListByCriteria();
+        return new ResponseEntity<>(ResponseDto.success("success", result), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/trust-grade/user/{userId}")
+    public ResponseEntity<?> getUserPossibleGrades(@PathVariable(name = "userId") Long userId) {
+        List<TrustGradeInfoResponseDto> result = trustGradeService.getPossibleUserGrades(userId);
         return new ResponseEntity<>(ResponseDto.success("success", result), HttpStatus.OK);
     }
 }

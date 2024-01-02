ea49bbd0e61503546fd44e4c57aa6cd6bfaa8fdd
songhyeonpk <ehftozl1997@gmail.com>
2024-01-02 22:13:15 +0900

    [feature/304-participating-my-project-list] feat : 회원이 참여중인 프로젝트 목록을 조회하는 요청 api 추가

--- src/main/java/com/example/demo/controller/project/ProjectController.java
+++ src/main/java/com/example/demo/controller/project/ProjectController.java
@@ -11,6 +11,7 @@ import com.example.demo.service.project.ProjectMemberService;
 import com.example.demo.service.project.ProjectService;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -26,6 +27,14 @@ public class ProjectController {
     public final ProjectService projectService;
     public final ProjectFacade projectFacade;
     public final ProjectMemberService projectMemberService;
+
+    @GetMapping("/me/participating")
+    public ResponseEntity<ResponseDto<?>> getMyProjectsParticipates(
+            @AuthenticationPrincipal PrincipalDetails user,
+            @RequestParam("pageIndex") Optional<Integer> pageIndex,
+            @RequestParam("itemCount") Optional<Integer> itemCount) {
+        return new ResponseEntity<>(ResponseDto.success("success", projectFacade.getMyProjectsParticipates(user.getId(), pageIndex.orElse(0), itemCount.orElse(6))), HttpStatus.OK);
+    }
 
     @GetMapping("/me")
     public ResponseEntity<ResponseDto<?>> getMyProjects(

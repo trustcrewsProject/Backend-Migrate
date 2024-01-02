44622b3cf3cca9dcc8b6f4b0697107e9e73569b9
songhyeonpk <ehftozl1997@gmail.com>
2024-01-02 22:10:54 +0900

    [feature/304-participating-my-project-list] feat : 요청한 회원이 참여중인 프로젝트 이력목록을 페이징 처리하여 조회하는 쿼리 구현

--- src/main/java/com/example/demo/repository/user/UserProjectHistoryRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/user/UserProjectHistoryRepositoryImpl.java
@@ -4,7 +4,12 @@ import static com.example.demo.model.project.QProject.project;
 import static com.example.demo.model.user.QUser.user;
 import static com.example.demo.model.user.QUserProjectHistory.userProjectHistory;
 
+import com.example.demo.constant.UserProjectHistoryStatus;
 import com.example.demo.dto.user.response.UserProjectHistoryInfoResponseDto;
+import com.example.demo.model.project.QProjectMember;
+import com.example.demo.model.trust_grade.QTrustGrade;
+import com.example.demo.model.user.UserProjectHistory;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -52,5 +57,47 @@ public class UserProjectHistoryRepositoryImpl implements UserProjectHistoryRepos
                         .fetch();
 
         return content;
+    }
+
+    @Override
+    public Long countParticipatesUserProjectHistoryByUserId(Long userId) {
+        // 회원 참여중인 프로젝트 이력 개수 조회
+        return jpaQueryFactory
+                .select(userProjectHistory.count())
+                .from(userProjectHistory)
+                .where(getPredicateForUserAndUserProjectHistoryStatus(userId))
+                .fetchOne();
+    }
+
+    @Override
+    public List<UserProjectHistory> findAllUserParticipates(Long userId, Pageable pageable) {
+        QTrustGrade qTrustGrade = QTrustGrade.trustGrade;
+        QProjectMember qProjectMember = QProjectMember.projectMember;
+
+        List<UserProjectHistory> results = jpaQueryFactory
+                .selectFrom(userProjectHistory)
+                .join(userProjectHistory.project, project)
+                .join(project.trustGrade, qTrustGrade)
+                .join(project.projectMembers, qProjectMember)
+                .join(qProjectMember.user, user)
+                .where(getPredicateForUserAndUserProjectHistoryStatus(userId))
+                .orderBy(userProjectHistory.startDate.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return results;
+    }
+
+    private BooleanBuilder getPredicateForUserAndUserProjectHistoryStatus(Long userId) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if(userId != null) {
+            builder.and(user.id.eq(userId));
+        }
+
+        builder.and(userProjectHistory.status.eq(UserProjectHistoryStatus.PARTICIPATING));
+
+        return builder;
     }
 }

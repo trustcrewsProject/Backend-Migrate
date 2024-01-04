b64d0aceebcb3286ab3901ee606c4b39e63d8f48
songhyeonpk <ehftozl1997@gmail.com>
2024-01-04 16:14:43 +0900

    [feature/288-project-member-works-with-trust-score] feat : 특정 프로젝트에 할당된 프로젝트 멤버의 업무 이력과 업무 별 신뢰점수 내역을 페이징 처리해 조회하는 커스텀 쿼리 로직 추가

--- src/main/java/com/example/demo/repository/work/WorkRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/work/WorkRepositoryImpl.java
@@ -1,10 +1,14 @@
 package com.example.demo.repository.work;
 
+import com.example.demo.dto.projectmember.response.ProjectMemberWorksPaginationResponseDto;
+import com.example.demo.dto.projectmember.response.ProjectMemberWorkWithTrustScoreResponseDto;
 import com.example.demo.dto.work.response.WorkPaginationResponseDto;
 import com.example.demo.dto.work.response.WorkReadResponseDto;
 import com.example.demo.model.milestone.QMilestone;
 import com.example.demo.model.project.QProject;
 import com.example.demo.model.project.QProjectMember;
+import com.example.demo.model.trust_score.QTrustScoreHistory;
+import com.example.demo.model.trust_score.QTrustScoreType;
 import com.example.demo.model.user.QUser;
 import com.example.demo.model.work.QWork;
 import com.querydsl.core.types.Projections;
@@ -26,6 +30,8 @@ public class WorkRepositoryImpl implements WorkRepositoryCustom{
     private final QWork qWork = QWork.work;
     private final QUser qUser = QUser.user;
     private final QProjectMember qProjectMember = QProjectMember.projectMember;
+    private final QTrustScoreHistory qTrustScoreHistory = QTrustScoreHistory.trustScoreHistory;
+    private final QTrustScoreType qTrustScoreType = QTrustScoreType.trustScoreType;
 
     @Override
     public WorkPaginationResponseDto findWorkByProjectIdAndMilestoneIdOrderByStartDateAsc(Long projectId, Long milestoneId, Pageable pageable) {
@@ -63,6 +69,39 @@ public class WorkRepositoryImpl implements WorkRepositoryCustom{
         long totalPages = getTotalItemCount(projectId, milestoneId, null);
 
         return WorkPaginationResponseDto.of(content, totalPages);
+    }
+
+    @Override
+    public ProjectMemberWorksPaginationResponseDto findWorksWithTrustScoreHistoryByProjectIdAndAssignedUserIdOrderByStartDateDesc(Long projectId, Long assignedUserId, Pageable pageable) {
+        // 프로젝트 멤버 업무 목록 + 업무 별 신뢰점수 내역 조회
+        List<ProjectMemberWorkWithTrustScoreResponseDto> content = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                ProjectMemberWorkWithTrustScoreResponseDto.class,
+                                qWork.content,
+                                qWork.startDate,
+                                qWork.endDate,
+                                qWork.progressStatus,
+                                qTrustScoreType.score,
+                                qTrustScoreType.gubunCode
+                        )
+                )
+                .from(qWork)
+                .leftJoin(qTrustScoreHistory).on(qWork.id.eq(qTrustScoreHistory.workId))
+                .leftJoin(qTrustScoreType).on(qTrustScoreHistory.trustScoreTypeId.eq(qTrustScoreType.id))
+                .where(
+                        eqProjectId(projectId),
+                        eqAssignedUserId(assignedUserId)
+                )
+                .orderBy(qWork.startDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        // 업무 전체 갯수 조회
+        long totalPages = getTotalItemCount(projectId, null, assignedUserId);
+
+        return ProjectMemberWorksPaginationResponseDto.of(content, totalPages);
     }
 
     // 조건에 맞는 업무의 총 개수 조회

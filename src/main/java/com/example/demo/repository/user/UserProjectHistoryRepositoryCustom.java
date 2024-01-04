0f8599f34be4bd21bb81f0f8bdeb8f89c6239b1e
songhyeonpk <ehftozl1997@gmail.com>
2024-01-04 17:14:48 +0900

    [feature/314-fix-get-user-project-history-response] fix : 사용자의 프로젝트 이력을 조회하는 쿼리에 이력 전체 개수를 조회하고 UserProjectHistoryPaginationResponseDto에 셋팅해 반환하도록 수정

--- src/main/java/com/example/demo/repository/user/UserProjectHistoryRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/user/UserProjectHistoryRepositoryCustom.java
@@ -3,17 +3,18 @@ package com.example.demo.repository.user;
 import com.example.demo.dto.user.response.UserProjectHistoryInfoResponseDto;
 import java.util.List;
 
+import com.example.demo.dto.user.response.UserProjectHistoryPaginationResponseDto;
 import com.example.demo.model.user.UserProjectHistory;
 import org.springframework.data.domain.Pageable;
 
 public interface UserProjectHistoryRepositoryCustom {
 
+    // 회원 프로젝트 이력 목록 조회 (수정날짜 기준 최신순 정렬, 페이징 offset 방법 활용)
+    UserProjectHistoryPaginationResponseDto findAllByUserIdOrderByUpdateDateDesc(
+            Long userId, Pageable pageable);
+
     // 회원 프로젝트 이력 전체 개수 조회
     Long countUserProjectHistoryByUserId(Long userId);
-
-    // 회원 프로젝트 이력 목록 조회 (수정날짜 기준 최신순 정렬, 페이징 offset 방법 활용)
-    List<UserProjectHistoryInfoResponseDto> findAllByUserIdOrderByUpdateDateDesc(
-            Long userId, Pageable pageable);
 
     // 회원 참여중인 프로젝트 이력 개수 조회
     Long countParticipatesUserProjectHistoryByUserId(Long userId);

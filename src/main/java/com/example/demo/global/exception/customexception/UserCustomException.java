7ed7d1b061b246885bbe4460353c897b8c26e4d8
박송현 <songhyeon@shyeon-MacBookAir.local>
2024-01-16 21:18:11 +0900

    [feature/331-my-profile-img-delete] add : 회원 프로필 이미지 관련 에러코드 및 커스텀 예외 추가

--- src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/UserCustomException.java
@@ -13,6 +13,9 @@ public class UserCustomException extends CustomException {
     public static final UserCustomException ALREADY_NICKNAME =
             new UserCustomException(UserErrorCode.ALREADY_NICKNAME);
 
+    public static final UserCustomException DOES_NOT_EXIST_PROFILE_IMG =
+            new UserCustomException(UserErrorCode.DOES_NOT_EXIST_PROFILE_IMG);
+
     public static final UserCustomException INVALID_AUTHENTICATION =
             new UserCustomException(UserErrorCode.INVALID_AUTHENTICATION);
 

32ae3d32b34f427ffe4f4f0d954b51ab96fe961d
songhyeonpk <ehftozl1997@gmail.com>
2024-01-26 17:53:42 +0900

    [feature/350-supported-project-alerts-by-user] feat : 사용자가 지원한 프로젝트 알림목록 조회 쿼리 구현

--- src/main/java/com/example/demo/repository/alert/AlertRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/alert/AlertRepositoryImpl.java
@@ -1,15 +1,20 @@
 package com.example.demo.repository.alert;
 
+import com.example.demo.constant.AlertType;
 import com.example.demo.dto.alert.response.AlertInfoResponseDto;
+import com.example.demo.dto.alert.response.AlertSupportedProjectInfoResponseDto;
 import com.example.demo.dto.common.PaginationResponseDto;
 import com.example.demo.dto.position.response.PositionInfoResponseDto;
+import com.example.demo.dto.project.response.ProjectSimpleInfoResponseDto;
 import com.example.demo.model.alert.QAlert;
 import com.example.demo.model.milestone.QMilestone;
 import com.example.demo.model.position.QPosition;
 import com.example.demo.model.project.QProject;
 import com.example.demo.model.user.QUser;
 import com.example.demo.model.work.QWork;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
@@ -66,18 +71,70 @@ public class AlertRepositoryImpl implements AlertRepositoryCustom{
                 .limit(pageable.getPageSize())
                 .fetch();
 
-        long totalPages = getAlertsTotalItemCount(projectId);
+        long totalPages = getAlertsTotalItemCount(projectId, null, null);
+
+        return PaginationResponseDto.of(content, totalPages);
+    }
+
+    @Override
+    public PaginationResponseDto findAlertsBySendUserIdAndTypeOrderByCreateDateDesc(Long sendUserId, Pageable pageable) {
+        List<AlertSupportedProjectInfoResponseDto> content = jpaQueryFactory
+                .select(
+                        Projections.constructor(
+                                AlertSupportedProjectInfoResponseDto.class,
+                                Projections.constructor(
+                                        ProjectSimpleInfoResponseDto.class,
+                                        qProject.id,
+                                        qProject.name
+                                ),
+                                Projections.constructor(
+                                        PositionInfoResponseDto.class,
+                                        qPosition.id,
+                                        qPosition.name
+                                ),
+                                qAlert.projectConfirmResult
+                        )
+                )
+                .from(qAlert)
+                .join(qAlert.project, qProject)
+                .join(qAlert.position, qPosition)
+                .join(qAlert.sendUser, qSendUser)
+                .where(qSendUser.id.eq(sendUserId),
+                        qAlert.type.eq(AlertType.RECRUIT))
+                .orderBy(qAlert.createDate.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long totalPages = getAlertsTotalItemCount(null, sendUserId, AlertType.RECRUIT);
 
         return PaginationResponseDto.of(content, totalPages);
     }
 
     // 알람 전체 개수 조회 (조건)
-    private long getAlertsTotalItemCount(Long projectId) {
+    private long getAlertsTotalItemCount(Long projectId, Long sendUserId, AlertType alertType) {
         return jpaQueryFactory
                 .select(qAlert.count())
                 .from(qAlert)
-                .join(qAlert.project, qProject)
-                .where(qAlert.project.id.eq(projectId))
+                .where(eq(projectId, sendUserId, alertType))
                 .fetchOne();
+    }
+
+    private BooleanBuilder eq(Long projectId, Long sendUserId, AlertType alertType) {
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if(projectId != null) {
+            builder.and(qAlert.project.id.eq(projectId));
+        }
+
+        if(sendUserId != null) {
+            builder.and(qAlert.sendUser.id.eq(sendUserId));
+        }
+
+        if(alertType != null) {
+            builder.and(qAlert.type.eq(alertType));
+        }
+
+        return builder;
     }
 }

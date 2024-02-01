02eda38b06273e04887732d7d135ba0b43b9c11d
songhyeonpk <ehftozl1997@gmail.com>
2024-02-01 16:17:43 +0900

    [feature/fix-project-withdraw-confirm] feat : 프로젝트, 회원 정보로 회원 프로젝트 이력을 조회하는 로직 추가

--- src/main/java/com/example/demo/repository/user/UserProjectHistoryRepository.java
+++ src/main/java/com/example/demo/repository/user/UserProjectHistoryRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo.repository.user;
 
+import com.example.demo.model.project.Project;
+import com.example.demo.model.user.User;
 import com.example.demo.model.user.UserProjectHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserProjectHistoryRepository
-        extends JpaRepository<UserProjectHistory, Long>, UserProjectHistoryRepositoryCustom {}
+        extends JpaRepository<UserProjectHistory, Long>, UserProjectHistoryRepositoryCustom {
+
+    Optional<UserProjectHistory> findUserProjectHistoryByProjectAndUser(Project project, User user);
+}

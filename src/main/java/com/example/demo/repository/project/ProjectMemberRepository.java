4e4ca4cdd9e02ffac8a2df4999a6595c72ceb923
songhyeonpk <ehftozl1997@gmail.com>
2024-02-02 17:00:53 +0900

    [feature/374-fix-get-project-members] feat : 프로젝트 정보와 프로젝트 멤버 상태로 멤버 목록을 조회하는 쿼리 추가

--- src/main/java/com/example/demo/repository/project/ProjectMemberRepository.java
+++ src/main/java/com/example/demo/repository/project/ProjectMemberRepository.java
@@ -1,14 +1,19 @@
 package com.example.demo.repository.project;
 
+import com.example.demo.constant.ProjectMemberStatus;
 import com.example.demo.model.project.Project;
 import com.example.demo.model.project.ProjectMember;
 import com.example.demo.model.user.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     Optional<List<ProjectMember>> findProjectsMemberByProject(Project project);
 
     Optional<ProjectMember> findProjectMemberByProjectAndUser(Project project, User user);
+
+    @Query("select pm from ProjectMember pm where pm.project = :project and pm.status = :status")
+    List<ProjectMember> findByProjectAndStatus(Project project, ProjectMemberStatus status);
 }

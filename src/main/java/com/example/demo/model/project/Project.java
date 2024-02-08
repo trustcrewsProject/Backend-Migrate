fd58e2740ec8fad035c08cc639d3eea0c22fb1bf
songhyeonpk <ehftozl1997@gmail.com>
2024-02-08 15:55:16 +0900

    [feature/fix-project-member-forced-withdrawal] feat : 프로젝트에서 멤버 삭제 로직 추가

--- src/main/java/com/example/demo/model/project/Project.java
+++ src/main/java/com/example/demo/model/project/Project.java
@@ -100,4 +100,8 @@ public class Project extends BaseTimeEntity {
     public void removeProjectTechnologies() {
         this.projectTechnologies.clear();
     }
+
+    public void removeProjectMember(ProjectMember projectMember) {
+        this.projectMembers.remove(projectMember);
+    }
 }

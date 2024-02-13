db49d0b7db6570b8b9ebe14d62b5d230e3dffdd7
songhyeonpk <ehftozl1997@gmail.com>
2024-02-13 15:33:04 +0900

    [feature/386-security-oauth] feat : OAuth 회원을 조회하는 쿼리 로직 추가

--- src/main/java/com/example/demo/repository/user/UserRepository.java
+++ src/main/java/com/example/demo/repository/user/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.demo.repository.user;
 
+import com.example.demo.constant.OAuthProvider;
 import com.example.demo.model.user.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long>, UserRepositoryCustom {
 
@@ -14,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, Long>, UserRepositor
 
     // 이메일로 회원 조화
     Optional<User> findByEmail(String email);
+
+    // provider, provider_id로 회원조회
+    @Query(value = "select u from User u where u.oAuthProvider = :provider and u.oAuthProviderId = :providerId")
+    Optional<User> findByProviderAndProviderId(OAuthProvider provider, String providerId);
 }

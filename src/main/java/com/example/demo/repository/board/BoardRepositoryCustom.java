c8a67afa34788a9a39e45d5cbecc79bfa6a7554b
songhyeonpk <ehftozl1997@gmail.com>
2024-02-13 21:16:58 +0900

    [feature/415-fix-board-search] fix : 게시글 검색 조회 쿼리에 게시글 모집상태, 게시글 프로젝트 상태에 대한 조건 추가

--- src/main/java/com/example/demo/repository/board/BoardRepositoryCustom.java
+++ src/main/java/com/example/demo/repository/board/BoardRepositoryCustom.java
@@ -2,11 +2,12 @@ package com.example.demo.repository.board;
 
 import java.util.List;
 
+import com.example.demo.constant.ProjectStatus;
 import com.example.demo.dto.common.PaginationResponseDto;
 import org.springframework.data.domain.Pageable;
 
 public interface BoardRepositoryCustom {
 
     PaginationResponseDto getBoardSearchPage(
-            Long positionId, String keyword, List<Long> technologyIds, Pageable pageable);
+            Long positionId, String keyword, List<Long> technologyIds, Boolean recruitmentStatus, ProjectStatus status, Pageable pageable);
 }

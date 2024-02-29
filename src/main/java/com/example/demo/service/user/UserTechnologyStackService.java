09a80084a50a1938c9bd80eb33956816ee946efe
songhyeonpk <ehftozl1997@gmail.com>
2024-02-29 16:31:35 +0900

    [cleanup/remove-unused-methods] remove : UserTechnologyStackService에서 사용하지 않는 메소드 목록 삭제

--- src/main/java/com/example/demo/service/user/UserTechnologyStackService.java
+++ src/main/java/com/example/demo/service/user/UserTechnologyStackService.java
@@ -5,12 +5,4 @@ import com.example.demo.model.user.User;
 import com.example.demo.model.user.UserTechnologyStack;
 import java.util.List;
 
-public interface UserTechnologyStackService {
-
-    // 회원 기술스택 목록 저장 및 반환
-    List<UserTechnologyStack> saveUserTechStacksAndReturnResponse(
-            User user, List<TechnologyStack> technologyStackList);
-
-    // 회원 기술스택 목록 삭제
-    void deleteUserTechStacks(List<UserTechnologyStack> technologyStackList);
-}
+public interface UserTechnologyStackService { }

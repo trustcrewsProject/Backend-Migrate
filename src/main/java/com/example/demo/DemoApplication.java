51c623470f5b2c3196ee9ae0967ae8883ef931f3
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-07-06 01:36:02 +0900

    style: 라인 수정

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing // jpa, 생성일, 마지막 수정일 추가
 public class DemoApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }

6139d425c0ba8d0fe0de8ef2154658d51cf1f9b8
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-08-22 20:25:15 +0900

    fix: 프로젝트 > 프로젝트 멤버 조회시 참여중인 멤버만 조회하도록 수정

--- src/main/java/com/example/demo/repository/project/ProjectMemberRepository.java
+++ src/main/java/com/example/demo/repository/project/ProjectMemberRepository.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
     Optional<List<ProjectMember>> findProjectsMemberByProject(Project project);
 
+    @Query("select pm from ProjectMember pm where pm.project = :project and pm.user = :user and pm.status = 'PARTICIPATING'")
     Optional<ProjectMember> findProjectMemberByProjectAndUser(Project project, User user);
 
     @Query("select pm from ProjectMember pm where pm.project = :project and pm.status = :status")
@@ -21,12 +22,12 @@ public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Lo
     @Query("select pm from ProjectMember pm where pm.user = :user and pm.status = :status")
     List<ProjectMember> findAllByUserAndStatus(User user, ProjectMemberStatus status);
 
-    @Query("SELECT COUNT(pm) from ProjectMember pm WHERE pm.projectMemberAuth.id != 4L AND pm.project = :project")
+    @Query("SELECT COUNT(pm) from ProjectMember pm WHERE pm.projectMemberAuth.id != 4L AND pm.status = 'PARTICIPATING' AND pm.project = :project")
     int countVotableProjectMember(Project project);
 
-    @Query("SELECT COUNT(pm) from ProjectMember pm WHERE pm.id != :fwMemberId AND pm.project.id = :projectId")
+    @Query("SELECT COUNT(pm) from ProjectMember pm WHERE pm.id != :fwMemberId AND pm.status = 'PARTICIPATING' AND pm.project.id = :projectId")
     int countFWVotableProjectMember(Long fwMemberId, Long projectId);
 
-    @Query("SELECT pm from ProjectMember pm WHERE pm.project.id = :projectId AND pm.user.id = :userId")
+    @Query("SELECT pm from ProjectMember pm WHERE pm.project.id = :projectId AND pm.user.id = :userId AND pm.status = 'PARTICIPATING'")
     ProjectMember findProjectMemberByPrIdAndUserId(Long projectId, Long userId);
 }

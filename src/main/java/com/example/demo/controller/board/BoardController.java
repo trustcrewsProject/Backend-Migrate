59ba1a032c471d8f94a1a977b0ca449989211c98
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-09-05 18:02:57 +0900

    fix: 프로젝트&모집게시글 생성 응답 메세지 수정

--- src/main/java/com/example/demo/controller/board/BoardController.java
+++ src/main/java/com/example/demo/controller/board/BoardController.java
@@ -2,16 +2,12 @@ package com.example.demo.controller.board;
 
 import com.example.demo.dto.board.Response.BoardTotalDetailResponseDto;
 import com.example.demo.dto.board_project.request.BoardProjectCreateRequestDto;
-import com.example.demo.dto.board_project.request.BoardProjectUpdateRequestDto;
 import com.example.demo.dto.board_project.response.BoardProjectCreateResponseDto;
-import com.example.demo.dto.board_project.response.BoardProjectUpdateResponseDto;
 import com.example.demo.dto.common.PaginationResponseDto;
 import com.example.demo.dto.common.ResponseDto;
 import com.example.demo.security.custom.PrincipalDetails;
 import com.example.demo.service.board.BoardFacade;
 import com.example.demo.service.board.BoardService;
-import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +15,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -51,29 +50,14 @@ public class BoardController {
             @AuthenticationPrincipal PrincipalDetails user,
             @RequestBody BoardProjectCreateRequestDto requestDto) {
         BoardProjectCreateResponseDto result = boardFacade.create(user.getId(), requestDto);
-        return new ResponseEntity<>(ResponseDto.success("게시글을 생성하였습니다.", result), HttpStatus.OK);
-    }
-
-    @PatchMapping("/{boardId}")
-    public ResponseEntity<ResponseDto<?>> update(
-            @AuthenticationPrincipal PrincipalDetails user,
-            @PathVariable("boardId") Long boardId,
-            @RequestBody BoardProjectUpdateRequestDto requestDto) {
-        try {
-            BoardProjectUpdateResponseDto result =
-                    boardFacade.update(user.getId(), boardId, requestDto);
-            return new ResponseEntity<>(ResponseDto.success("게시글을 수정하였습니다.", result), HttpStatus.OK);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return new ResponseEntity<>(ResponseDto.fail(ex.getMessage()), HttpStatus.BAD_REQUEST);
-        }
+        return new ResponseEntity<>(ResponseDto.success("프로젝트와 모집 게시글을 생성했습니다.", result), HttpStatus.OK);
     }
 
     @DeleteMapping("/{boardId}")
     public ResponseEntity<ResponseDto<?>> delete(
             @AuthenticationPrincipal PrincipalDetails user, @PathVariable("boardId") Long boardId) {
         boardFacade.deleteBoard(user.getId(), boardId);
-        return new ResponseEntity<>(ResponseDto.success("게시글을 삭제하였습니다.", null), HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(ResponseDto.success("게시글을 삭제했습니다.", null), HttpStatus.NO_CONTENT);
     }
 
     @PatchMapping("/{boardId}/recruitment-status")

9bf1fc6d1b78e8837a4c5dd3e742a0d3b2a5e596
김은선 <60612115+EUNSUN23@users.noreply.github.com>
2024-11-25 15:35:39 +0900

    fix: 프로젝트 상세 > 마일스톤생성 권한없음응답 에러처리 지시문 수정

--- src/main/java/com/example/demo/global/exception/customexception/ProjectCustomException.java
+++ src/main/java/com/example/demo/global/exception/customexception/ProjectCustomException.java
@@ -6,13 +6,13 @@ import com.example.demo.global.exception.errorcode.ProjectErrorCode;
 public class ProjectCustomException extends CustomExceptionWithInstruct {
 
     public static final ProjectCustomException NOT_FOUND_PROJECT =
-            new ProjectCustomException(ProjectErrorCode.NOT_FOUND_PROJECT, ErrorInstruction.NONE);
+            new ProjectCustomException(ProjectErrorCode.NOT_FOUND_PROJECT, ErrorInstruction.REDIRECT);
 
     public static final ProjectCustomException NO_PERMISSION_TO_TASK =
-            new ProjectCustomException(ProjectErrorCode.NO_PERMISSION_TO_TASK, ErrorInstruction.NONE);
+            new ProjectCustomException(ProjectErrorCode.NO_PERMISSION_TO_TASK, ErrorInstruction.MESSAGE);
 
     public static final ProjectCustomException ACCESS_NOT_ALLOWED =
-            new ProjectCustomException(ProjectErrorCode.ACCESS_NOT_ALLOWED, ErrorInstruction.NONE);
+            new ProjectCustomException(ProjectErrorCode.ACCESS_NOT_ALLOWED, ErrorInstruction.REDIRECT);
 
     public ProjectCustomException(ProjectErrorCode projectErrorCode, ErrorInstruction errorInstruction) {
         super(projectErrorCode, errorInstruction);
--- src/main/java/com/example/demo/global/exception/errorcode/ProjectErrorCode.java
+++ src/main/java/com/example/demo/global/exception/errorcode/ProjectErrorCode.java
@@ -5,9 +5,9 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ProjectErrorCode implements ErrorCode {
-    NOT_FOUND_PROJECT(HttpStatus.NOT_FOUND, "NOT_FOUND_PROJECT"),
-    ACCESS_NOT_ALLOWED(HttpStatus.FORBIDDEN, "ACCESS_NOT_ALLOWED"),
-    NO_PERMISSION_TO_TASK(HttpStatus.FORBIDDEN, "NO_PERMISSION_TO_TASK");
+    NOT_FOUND_PROJECT(HttpStatus.NOT_FOUND, "프로젝트가 존재하지 않습니다."),
+    ACCESS_NOT_ALLOWED(HttpStatus.FORBIDDEN, "요청한 정보에 대한 접근 권한이 없습니다."),
+    NO_PERMISSION_TO_TASK(HttpStatus.FORBIDDEN, "요청한 작업에 대한 수행 권한이 없습니다.");
 
     private HttpStatus status;
     private String message;
